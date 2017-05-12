701f3b60423864548e0a9d7827b9efec661dafa2
Filip Kudla <filip.kudla@snow.dog>
2017-05-12 13:25:24 +0200

    #29696 - Add yarn script + repair gulpfile warnings

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,7 @@
+/* eslint no-inline-comments: 0 */
+/* eslint one-var: 0 */
+/* eslint-env es6 */
+
 const autoprefixer = require('autoprefixer'),
       eslint       = require('gulp-eslint'),
       fractal      = require('@frctl/fractal').create(),
@@ -16,14 +20,14 @@ const autoprefixer = require('autoprefixer'),
       util         = require('gulp-util');
 
 const processors = [
-    autoprefixer()
+  autoprefixer()
 ];
 
 const paths = {
-    components: 'components',
-    css : 'public/css',
-    maps: 'cssmaps',
-    sass: 'docs/styles.scss',
+  components: 'components',
+  css : 'public/css',
+  maps: 'cssmaps',
+  sass: 'docs/styles.scss',
 }
 
 // Fractal configuration
@@ -34,9 +38,9 @@ fractal.docs.set('path', `${__dirname}/docs`); // location of the documentation 
 fractal.components.set('path', `${__dirname}/components`); // location of the component directory.
 
 fractal.web.theme(
-    mandelbrot({
-        skin: "black"
-    })
+  mandelbrot({
+    skin: 'black'
+  })
 );
 
 const logger = fractal.cli.console; // keep a reference to the fractal CLI console utility
@@ -44,100 +48,102 @@ const logger = fractal.cli.console; // keep a reference to the fractal CLI conso
 // get handlebars component engine (default) and register partial
 const hbsEngine = fractal.components.engine();
 
-hbsEngine.handlebars.registerHelper('pager', function (context, options) {
-    var ret = "";
+hbsEngine.handlebars.registerHelper('pager', function(context, options) {
+  let ret = '';
 
-    function findIndexByActive(source, active) {
-        for (var i = 0; i < source.length; i++) {
-            if (source[i].active === active) {
-                return i;
-            }
-        }
+  function findIndexByActive(source, active) {
+    for (let i = 0; i < source.length; i++) {
+      if (source[i].active === active) {
+        return i;
+      }
     }
+  }
 
-    if (context.length > 6) {
-        var result = findIndexByActive(context, true),
-            arrayLength = context.length,
-            newContext = new Array(),
-            middlePage = {
-                pageNo: '...',
-                active: false
-            };
+  if (context.length > 6) {
+    let result = findIndexByActive(context, true),
+        arrayLength = context.length,
+        newContext = new Array(),
+        middlePage = {
+          pageNo: '...',
+          active: false
+        };
 
-        if ((arrayLength - result) > 6) {
+    if ((arrayLength - result) > 6) {
 
-            for (var k = result; k < (result + 3); k++) {
-                newContext.push(context[k])
-            }
-            newContext.push(middlePage);
-            newContext.push(context[(context.length - 2)], context[(context.length - 1)]);
+      for (let k = result; k < (result + 3); k++) {
+        newContext.push(context[k])
+      }
+      newContext.push(middlePage);
+      newContext.push(context[(context.length - 2)], context[(context.length - 1)]);
 
-            for (var i = 0, j = newContext.length; i < j; i++) {
-                ret = ret + options.fn(newContext[i]);
-            }
+      for (let i = 0, j = newContext.length; i < j; i++) {
+        ret = ret + options.fn(newContext[i]);
+      }
 
-        } else {
-            for (var k = result; k < (result + 5); k++) {
-                newContext.push(context[k]);
-            }
-
-            for (var i = 0, j = newContext.length; i < j; i++) {
-                ret = ret + options.fn(newContext[i]);
-            }
-        }
-    } else {
-        for (var i = 0, j = context.length; i < j; i++) {
-            ret = ret + options.fn(context[i]);
-        }
     }
+    else {
+      for (let k = result; k < (result + 5); k++) {
+        newContext.push(context[k]);
+      }
 
-    return ret;
+      for (let i = 0, j = newContext.length; i < j; i++) {
+        ret = ret + options.fn(newContext[i]);
+      }
+    }
+  }
+  else {
+    for (let i = 0, j = context.length; i < j; i++) {
+      ret = ret + options.fn(context[i]);
+    }
+  }
+
+  return ret;
 });
 
-hbsEngine.handlebars.registerHelper("lastActive", function (array) {
-    var last = array[array.length - 1];
-    return last.active;
+hbsEngine.handlebars.registerHelper('lastActive', function(array) {
+  let last = array[array.length - 1];
+  return last.active;
 });
 
-hbsEngine.handlebars.registerHelper('tabList', function (context, options) {
-    var ret = '<ul class="tabs__list">';
+hbsEngine.handlebars.registerHelper('tabList', function(context, options) {
+  let ret = '<ul class="tabs__list">';
 
-    for (var i = 0, j = context.length; i < j; i++) {
-        ret = ret + '<li class="tabs__list-item">' + options.fn(context[i]) + "</li>";
-    }
+  for (let i = 0, j = context.length; i < j; i++) {
+    ret = ret + '<li class="tabs__list-item">' + options.fn(context[i]) + '</li>';
+  }
 
-    return ret + "</ul>";
+  return ret + '</ul>';
 });
 
-hbsEngine.handlebars.registerHelper('jsPath', function (componentName) {
+hbsEngine.handlebars.registerHelper('jsPath', function(componentName) {
   return '/components/raw/' + componentName + '/' + componentName + '.js';
 });
 
-hbsEngine.handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
-    switch (operator) {
-        case '==':
-            return (v1 == v2) ? options.fn(this) : options.inverse(this);
-        case '===':
-            return (v1 === v2) ? options.fn(this) : options.inverse(this);
-        case '!=':
-            return (v1 != v2) ? options.fn(this) : options.inverse(this);
-        case '!==':
-            return (v1 !== v2) ? options.fn(this) : options.inverse(this);
-        case '<':
-            return (v1 < v2) ? options.fn(this) : options.inverse(this);
-        case '<=':
-            return (v1 <= v2) ? options.fn(this) : options.inverse(this);
-        case '>':
-            return (v1 > v2) ? options.fn(this) : options.inverse(this);
-        case '>=':
-            return (v1 >= v2) ? options.fn(this) : options.inverse(this);
-        case '&&':
-            return (v1 && v2) ? options.fn(this) : options.inverse(this);
-        case '||':
-            return (v1 || v2) ? options.fn(this) : options.inverse(this);
-        default:
-            return options.inverse(this);
-    }
+hbsEngine.handlebars.registerHelper('ifCond', function(v1, operator, v2, options) {
+  switch (operator) {
+  case '==':
+    return (v1 == v2) ? options.fn(this) : options.inverse(this);
+  case '===':
+    return (v1 === v2) ? options.fn(this) : options.inverse(this);
+  case '!=':
+    return (v1 != v2) ? options.fn(this) : options.inverse(this);
+  case '!==':
+    return (v1 !== v2) ? options.fn(this) : options.inverse(this);
+  case '<':
+    return (v1 < v2) ? options.fn(this) : options.inverse(this);
+  case '<=':
+    return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+  case '>':
+    return (v1 > v2) ? options.fn(this) : options.inverse(this);
+  case '>=':
+    return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+  case '&&':
+    return (v1 && v2) ? options.fn(this) : options.inverse(this);
+  case '||':
+    return (v1 || v2) ? options.fn(this) : options.inverse(this);
+  default:
+    return options.inverse(this);
+  }
 });
 
 hbsEngine.handlebars.registerHelper('inline', src => fs.readFileSync(src, 'utf8'));
@@ -145,73 +151,81 @@ hbsEngine.handlebars.registerHelper('inline', src => fs.readFileSync(src, 'utf8'
 // Fractal gulp tasks
 
 gulp.task('fractal:start', ['sass', 'watch'], () => {
-    const server = fractal.web.server({
-        sync: true,
-        port: 4000
-    });
-    server.on('error', err => logger.error(err.message));
+  const server = fractal.web.server({
+    sync: true,
+    port: 4000
+  });
+  server.on('error', err => logger.error(err.message));
 
-    return server.start().then(() => {
-        logger.success(`Fractal server is now running at ${server.url}`);
-    });
+  return server.start().then(() => {
+    logger.success(`Fractal server is now running at ${server.url}`);
+  });
 });
 
 gulp.task('fractal:build', ['sass'], () => {
-    const builder = fractal.web.builder();
-    builder.on('progress', (completed, total) => logger.update(`Exported ${completed} of ${total} items`, 'info'));
-    builder.on('error', err => logger.error(err.message));
+  const builder = fractal.web.builder();
+  builder.on('progress', (completed, total) => logger.update(`Exported ${completed} of ${total} items`, 'info'));
+  builder.on('error', err => logger.error(err.message));
 
-    return builder.build().then(() => {
-        logger.success('Fractal build completed!');
-    });
+  return builder.build().then(() => {
+    logger.success('Fractal build completed!');
+  });
 });
 
 // Gulp tasks
 
 gulp.task('watch', () => {
-    gulp.watch('**/*.scss', () => {
-        gulp.src(paths.scss)
-            .pipe(plumber())
-            .pipe(sourcemaps.init())
-            .pipe(sass({outputStyle: 'compressed'}))
-            .pipe(postcss(processors))
-            .pipe(sourcemaps.write(paths.maps))
-            .pipe(gulp.dest(paths.css))
+  gulp.watch('**/*.scss', () => {
+    gulp.src(paths.scss)
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(sass(
+      {
+        outputStyle: 'compressed'
+      }
+    ))
+    .pipe(postcss(processors))
+    .pipe(sourcemaps.write(paths.maps))
+    .pipe(gulp.dest(paths.css))
 
-        runSequence('sass-lint', 'sass', 'css-lint');
-    })
+    runSequence('sass-lint', 'sass', 'css-lint');
+  })
 });
 
 gulp.task('sass', () => {
-    gulp.src('docs/styles.scss')
-        .pipe(plumber())
-        .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(postcss(processors))
-        .pipe(sourcemaps.write(paths.maps))
-        .pipe(gulp.dest(paths.css))
+  gulp.src('docs/styles.scss')
+  .pipe(plumber())
+  .pipe(sourcemaps.init())
+  .pipe(sass(
+    {
+      outputStyle: 'compressed'
+    }
+  ))
+  .pipe(postcss(processors))
+  .pipe(sourcemaps.write(paths.maps))
+  .pipe(gulp.dest(paths.css))
 });
 
 gulp.task('sass-lint', () => {
-    return gulp.src(paths.components + '/**/*.scss')
-        .pipe(sassLint())
-        .pipe(sassLint.format())
-        .pipe(gulpif(util.env.ci, sassLint.failOnError()));
+  return gulp.src(paths.components + '/**/*.scss')
+  .pipe(sassLint())
+  .pipe(sassLint.format())
+  .pipe(gulpif(util.env.ci, sassLint.failOnError()));
 });
 
 gulp.task('css-lint', () => {
-    return gulp.src(paths.css + '/**/*.css')
-        .pipe(postcss([
-            stylelint(),
-            reporter({
-                throwError: util.env.ci || false
-            })
-        ]));
+  return gulp.src(paths.css + '/**/*.css')
+  .pipe(postcss([
+    stylelint(),
+    reporter({
+      throwError: util.env.ci || false
+    })
+  ]));
 });
 
 gulp.task('js-lint', () => {
-    return gulp.src(paths.components + '/**/*.js')
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(gulpif(util.env.ci, eslint.failAfterError()));
+  return gulp.src(paths.components + '/**/*.js')
+  .pipe(eslint())
+  .pipe(eslint.format())
+  .pipe(gulpif(util.env.ci, eslint.failAfterError()));
 });
