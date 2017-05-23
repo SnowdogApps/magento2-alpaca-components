376ab49934d9889e205eeb072a18810a061c7d64
Filip Kudla <filip.kudla@snow.dog>
2017-05-23 15:25:58 +0200

    #29987 - Change JS with simplified from other components and rename tags

--- components/blocks/dropdown-list/dropdown-list.js
+++ components/blocks/dropdown-list/dropdown-list.js
@@ -1,7 +1,8 @@
 'use-strict'
-const dropdownItem      = document.querySelectorAll('.dropdown-list__item--parent > .dropdown-list__link'),
-      dropdownInnerItem = document.querySelectorAll('.dropdown-list__inner-item--parent > .dropdown-list__inner-link'),
-      alliTemLabel      = 'All products';
+const alliTemLabel      = 'All products',
+      currentInnerItem  = document.querySelector('.dropdown-list__inner-link.current'),
+      dropdownItem      = document.querySelectorAll('.dropdown-list__item--parent .dropdown-list__link'),
+      dropdownInnerItem = document.querySelectorAll('.dropdown-list__inner-item--parent .dropdown-list__inner-link');
 
 function setListHeight(item) {
   return Array.from(item.children)
@@ -9,18 +10,17 @@ function setListHeight(item) {
     .reduce((a, b) => a + b, 0);
 }
 
-function slideList(item, inner) {
+function toggleSubmenu(item, inner) {
+  const dropdownId = item.dataset.dropdown;
+  let dropdownList = document.querySelector(`.dropdown-list__inner-list--level1[data-dropdown="${dropdownId}"]`);
+  let innerLists = item.parentNode.querySelectorAll(`.dropdown-list__inner-list--level2`);
+  let upperList  = null;
 
-  const dropdownId = item.dataset.dropdown,
-        dropdownList = inner
-        ? item.parentNode.querySelector(`.dropdown-list__inner-list--level2[data-dropdown="${dropdownId}"]`)
-        : document.querySelector(`.dropdown-list__inner-list--level1[data-dropdown="${dropdownId}"]`),
-        innerLists = inner
-        ? null
-        : item.parentNode.querySelectorAll('.dropdown-list__inner-list--level2'),
-        upperList = inner
-        ? item.closest('.dropdown-list__inner-list--level1')
-        : null;
+  if (inner) {
+    dropdownList = item.parentNode.querySelector(`.dropdown-list__inner-list--level2[data-dropdown="${dropdownId}"]`);
+    innerLists = null;
+    upperList  = item.closest(`.dropdown-list__inner-list--level1`);
+  }
 
   item.parentNode.classList.toggle('open');
 
@@ -34,52 +34,47 @@ function slideList(item, inner) {
     else if (upperList) {
       upperList.style.height = upperList.clientHeight - dropdownList.clientHeight + 'px';
     }
+
     dropdownList.style.height = 0;
   }
   else {
-
     const listHeight = setListHeight(dropdownList);
     dropdownList.style.height = listHeight + 'px';
+
     if (upperList) {
       upperList.style.height = upperList.clientHeight + listHeight + 'px';
     }
   }
 }
 
-function appendAllItem(item) {
-  const itemHref = item.href,
-        sublist = document.querySelector(`ul[data-dropdown="${item.dataset.dropdown}"]`),
-        levelClass = sublist.classList.value.includes('level1')
-        ? ' dropdown-list__inner-item--level1'
-        : ' dropdown-list__inner-item--level2',
-        allLink = `
-          <a href="${itemHref}" class="dropdown-list__inner-link">
-            ${alliTemLabel}
-          </a>
-        `,
-        allItem = document.createElement('li');
+if (currentInnerItem !== null) {
+    const currentList       = currentInnerItem.closest('.filters__inner-list'),
+          currentListHeight = setListHeight(currentList);
+    let currentUpperList = null;
 
-  allItem.innerHTML = allLink;
-  allItem.classList = `dropdown-list__inner-item dropdown-list__inner-item--all ${levelClass}`;
-  sublist.insertBefore(allItem, sublist.firstChild);
+    if (currentList.classList.contains('filters__inner-list--level2')) {
+        currentUpperList = currentList.closest('.filters__inner-list--level1');
+    }
+
+    currentList.style.height = currentListHeight + 'px';
+    currentList.parentNode.classList.toggle('open');
+
+    if (currentUpperList) {
+        currentUpperList.style.height = currentUpperList.clientHeight + setListHeight(currentUpperList) + currentListHeight + 'px';
+        currentUpperList.parentNode.classList.toggle('open');
+    }
 }
 
 dropdownItem.forEach(
-  key => {
-    key.addEventListener('click', (e) => {
-      e.preventDefault();
-      slideList(key, false);
-    }, false);
-    appendAllItem(key);
-  }
+  key => key.addEventListener('click', (e) => {
+    e.preventDefault();
+    toggleSubmenu(key, false);
+  }, false)
 );
 
 dropdownInnerItem.forEach(
-  key => {
-    key.addEventListener('click', (e) => {
-      e.preventDefault();
-      slideList(key, true);
-    }, false);
-    appendAllItem(key);
-  }
+  key => key.addEventListener('click', (e) => {
+    e.preventDefault();
+    toggleSubmenu(key, true);
+  }, false)
 );
