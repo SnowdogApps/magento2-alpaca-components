acc659ddd1a17689ab4f9e2e6176542e02695a64
Filip Kudla <filip.kudla@snow.dog>
2017-05-23 10:57:10 +0200

    #29977 - Fix pager component and rewrite it + simplfiy version without helpers

--- components/blocks/pager/pager.config.js
+++ components/blocks/pager/pager.config.js
@@ -1,253 +1,158 @@
 module.exports = {
   title: 'Pager component',
   context: {
-    pages: [
+    types: [
       {
-        pageNo: 1,
-        active: true
+        text: 'Pager type with arrow in right',
+        arrowLeftVisible: false,
+        arrowRightVisible: true,
+        pages: [
+          {
+            pageNo: 1,
+            active: true
+          },
+          {
+            pageNo: 2,
+            active: false
+          },
+          {
+            pageNo: 3,
+            active: false
+          },
+          {
+            pageNo: 4,
+            active: false
+          },
+          {
+            pageNo: 5,
+            active: false
+          },
+          {
+            pageNo: 6,
+            active: false
+          }
+        ]
       },
       {
-        pageNo: 2,
-        active: false
+        text: 'Pager type with arrow in right and left',
+        arrowLeftVisible: true,
+        arrowRightVisible: true,
+        pages: [
+          {
+            pageNo: 1,
+            active: false
+          },
+          {
+            pageNo: 2,
+            active: false
+          },
+          {
+            pageNo: 3,
+            active: true
+          },
+          {
+            pageNo: 4,
+            active: false
+          },
+          {
+            pageNo: 5,
+            active: false
+          },
+          {
+            pageNo: 6,
+            active: false
+          }
+        ]
       },
       {
-        pageNo: 3,
-        active: false
+        text: 'Pager type with arrow in left',
+        arrowLeftVisible: true,
+        arrowRightVisible: false,
+        pages: [
+          {
+            pageNo: 1,
+            active: false
+          },
+          {
+            pageNo: 2,
+            active: false
+          },
+          {
+            pageNo: 3,
+            active: false
+          },
+          {
+            pageNo: 4,
+            active: false
+          },
+          {
+            pageNo: 5,
+            active: false
+          },
+          {
+            pageNo: 6,
+            active: true
+          }
+        ]
       },
       {
-        pageNo: 4,
-        active: false
+        text: 'Pager type with arrow in left and right with more sites',
+        arrowLeftVisible: true,
+        arrowRightVisible: true,
+        pages: [
+          {
+            pageNo: 6,
+            active: true
+          },
+          {
+            pageNo: 7,
+            active: false
+          },
+          {
+            pageNo: 8,
+            active: false
+          },
+          {
+            pageNo: '...',
+            active: false
+          },
+          {
+            pageNo: 17,
+            active: false
+          },
+          {
+            pageNo: 18,
+            active: false
+          }
+        ]
       },
       {
-        pageNo: 5,
-        active: false
-      },
-      {
-        pageNo: 6,
-        active: false
+        text: 'Pager type with arrow in right',
+        arrowLeftVisible: true,
+        arrowRightVisible: true,
+        pages: [
+          {
+            pageNo: 13,
+            active: true
+          },
+          {
+            pageNo: 14,
+            active: false
+          },
+          {
+            pageNo: 15,
+            active: false
+          },
+          {
+            pageNo: 16,
+            active: false
+          },
+          {
+            pageNo: 17,
+            active: false
+          }
+        ]
       }
     ]
-  },
-  variants: [
-    {
-      name: 'Middle page active',
-      context: {
-        pages: [
-          {
-            pageNo: 1,
-            active: false
-          },
-          {
-            pageNo: 2,
-            active: false
-          },
-          {
-            pageNo: 3,
-            active: true
-          },
-          {
-            pageNo: 4,
-            active: false
-          },
-          {
-            pageNo: 5,
-            active: false
-          },
-          {
-            pageNo: 6,
-            active: false
-          }
-        ]
-      }
-    },
-    {
-      name: 'Last page active',
-      context: {
-        pages: [
-          {
-            pageNo: 1,
-            active: false
-          },
-          {
-            pageNo: 2,
-            active: false
-          },
-          {
-            pageNo: 3,
-            active: false
-          },
-          {
-            pageNo: 4,
-            active: false
-          },
-          {
-            pageNo: 5,
-            active: false
-          },
-          {
-            pageNo: 6,
-            active: true
-          }
-        ]
-      }
-    },
-    {
-      name: 'Many pages variant 1',
-      context: {
-        pages: [
-          {
-            pageNo: 1,
-            active: false
-          },
-          {
-            pageNo: 2,
-            active: false
-          },
-          {
-            pageNo: 3,
-            active: false
-          },
-          {
-            pageNo: 4,
-            active: false
-          },
-          {
-            pageNo: 5,
-            active: false
-          },
-          {
-            pageNo: 6,
-            active: true
-          },
-          {
-            pageNo: 7,
-            active: false
-          },
-          {
-            pageNo: 8,
-            active: false
-          },
-          {
-            pageNo: 9,
-            active: false
-          },
-          {
-            pageNo: 10,
-            active: false
-          },
-          {
-            pageNo: 11,
-            active: false
-          },
-          {
-            pageNo: 12,
-            active: false
-          },
-          {
-            pageNo: 13,
-            active: false
-          },
-          {
-            pageNo: 14,
-            active: false
-          },
-          {
-            pageNo: 15,
-            active: false
-          },
-          {
-            pageNo: 16,
-            active: false
-          },
-          {
-            pageNo: 17,
-            active: false
-          },
-          {
-            pageNo: 18,
-            active: false
-          }
-        ]
-      }
-    },
-    {
-      name: 'Many pages variant 2',
-      context: {
-        pages: [
-          {
-            pageNo: 1,
-            active: false
-          },
-          {
-            pageNo: 2,
-            active: false
-          },
-          {
-            pageNo: 3,
-            active: false
-          },
-          {
-            pageNo: 4,
-            active: false
-          },
-          {
-            pageNo: 5,
-            active: false
-          },
-          {
-            pageNo: 6,
-            active: false
-          },
-          {
-            pageNo: 7,
-            active: false
-          },
-          {
-            pageNo: 8,
-            active: false
-          },
-          {
-            pageNo: 9,
-            active: false
-          },
-          {
-            pageNo: 10,
-            active: false
-          },
-          {
-            pageNo: 11,
-            active: false
-          },
-          {
-            pageNo: 12,
-            active: false
-          },
-          {
-            pageNo: 13,
-            active: true
-          },
-          {
-            pageNo: 14,
-            active: false
-          },
-          {
-            pageNo: 15,
-            active: false
-          },
-          {
-            pageNo: 16,
-            active: false
-          },
-          {
-            pageNo: 17,
-            active: false
-          },
-          {
-            pageNo: 18,
-            active: false
-          }
-        ]
-      }
-    }
-  ]
+  }
 };
