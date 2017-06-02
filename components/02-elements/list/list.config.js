7b8a215320555a44da25abe061921595bc39c914
Bartek Igielski <igloczek@gmail.com>
2017-06-03 00:12:38 +0200

    List component rewrite - part 1

--- components/02-elements/list/list.config.js
+++ components/02-elements/list/list.config.js
@@ -1,184 +1,52 @@
 module.exports = {
-  title: 'List',
   context: {
-    contentList: [
-      {
-        text: 'Some item text'
-      },
-      {
-        text: 'Some item text'
-      },
-      {
-        text: 'Some item text'
-      }
+    listTag: 'ul',
+    elementTag: 'li',
+    elements: [
+      'Lorem ipsum',
+      'Lorem ipsum',
+      'Lorem ipsum',
+      'Lorem ipsum',
+      'Lorem ipsum'
     ]
   },
   variants: [
     {
-      name: 'odd-even',
+      name: 'horizontal',
       context: {
-        modifier: 'odd-even'
+        modificators: 'list--horizontal'
       }
     },
     {
-      name: 'simple labeling',
+      name: 'icon',
       context: {
-        contentList: [
+        modificators: 'list--with-icon',
+        elements: [
           {
-            label: 'Label1:',
-            value: 'Value1',
-            text: false
+            icon: 'facebook',
+            text: 'Facebook'
           },
           {
-            label: 'Label2:',
-            value: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eleifend auctor neque posuere volutpat. Nunc quam lacus, ultricies id molestie vel, sodales a massa. Vivamus auctor arcu a orci sagittis',
-            text: false
+            icon: 'twitter',
+            text: 'Twitter'
           },
           {
-            label: 'Label2:',
-            value: 'Value2',
-            text: false
+            icon: 'linkedin',
+            text: 'Linkedin'
           }
         ]
       }
     },
     {
-      name: 'wide labeling',
+      name: 'image',
       context: {
-        modifier: 'labeling',
-        modifier2: 'odd-even',
-        contentList: [
-          {
-            label: 'Label text goes here',
-            value: 'value text',
-            text: false
-          },
-          {
-            label: 'Label text',
-            value: 'value text goes here',
-            text: false
-          },
-          {
-            label: 'Label text goes here',
-            value: 'value text goes here',
-            text: false
-          },
-          {
-            label: 'Label text',
-            value: 'value text',
-            text: false
-          },
-          {
-            label: 'Label text goes here',
-            value: 'value text',
-            text: false
-          },
-          {
-            label: 'Label text',
-            value: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-            text: false
-          }
-        ]
+        modificators: 'list--with-image'
       }
     },
     {
-      name: 'titled',
+      name: 'divided',
       context: {
-        modifier: 'titled',
-        modifier2: '2cols',
-        listTitle: 'List title',
-        listId: 'id1',
-        contentList: [
-          {
-            text: 'Page 1',
-            link: true
-          },
-          {
-            text: 'Page 2',
-            link: true
-          },
-          {
-            text: 'Page 3',
-            link: true
-          },
-          {
-            text: 'Custom page',
-            link: true
-          },
-          {
-            text: 'Custom page 1',
-            link: true
-          },
-          {
-            text: 'Custom page 2',
-            link: true
-          },
-          {
-            text: 'Custom page 3',
-            link: true
-          }
-        ]
-      }
-    },
-    {
-      name: 'search-categories',
-      context: {
-        modifier: 'search',
-        contentList: [
-          {
-            text: 'Category 1',
-            itemQty: '34',
-            link: true
-          },
-          {
-            text: 'Category 2',
-            itemQty: '24',
-            link: true
-          },
-          {
-            text: 'Category 3',
-            itemQty: '102',
-            link: true
-          },
-          {
-            text: 'Category 4',
-            itemQty: '4',
-            link: true
-          },
-          {
-            text: 'Category 5',
-            itemQty: '18',
-            link: true
-          }
-        ]
-      }
-    },
-    {
-      name: 'social',
-      context: {
-        modifier: 'social',
-        contentList: [
-          {
-            iconId: 'facebook',
-            iconClass: 'facebook',
-            iconLink: '#somelink'
-          },
-          {
-            iconId: 'twitter',
-            iconClass: 'twitter',
-            iconLink: '#somelink'
-          },
-          {
-            iconId: 'google-plus',
-            iconClass: 'google',
-            iconLink: '#somelink'
-          },
-          {
-            iconId: 'linkedin',
-            iconClass: 'linkedin',
-            iconLink: '#somelink'
-          }
-        ]
+        modificators: 'list--divided'
       }
     }
   ]
