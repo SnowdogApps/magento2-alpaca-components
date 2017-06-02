363dabb06d9863ec993ab34cbafd413d8a92f697
Bartek Igielski <igloczek@gmail.com>
2017-06-02 22:44:28 +0200

    Typography component update

--- components/01-globals/typography/heading/heading.config.js
+++ components/01-globals/typography/heading/heading.config.js
@@ -11,91 +11,11 @@ module.exports = {
       }
     },
     {
-      name: 'tag-2',
-      context: {
-        tag: 'h2',
-        class: '',
-        text: 'Second level heading - only tag'
-      }
-    },
-    {
-      name: 'tag-3',
-      context: {
-        tag: 'h3',
-        class: '',
-        text: 'Third level heading - only tag'
-      }
-    },
-    {
-      name: 'tag-4',
-      context: {
-        tag: 'h4',
-        class: '',
-        text: 'Fourth level heading - only tag'
-      }
-    },
-    {
-      name: 'tag-5',
-      context: {
-        tag: 'h5',
-        class: '',
-        text: 'Fifth level heading - only tag'
-      }
-    },
-    {
-      name: 'tag-6',
-      context: {
-        tag: 'h6',
-        class: '',
-        text: 'Sixth level heading - only tag'
-      }
-    },
-    {
       name: 'class-1',
       context: {
         tag: 'h1',
         class: 'heading heading--first-level',
         text: 'First level heading - only class'
-      }
-    },
-    {
-      name: 'class-2',
-      context: {
-        tag: 'h2',
-        class: 'heading heading--second-level',
-        text: 'Second level heading - only class'
-      }
-    },
-    {
-      name: 'class-3',
-      context: {
-        tag: 'h3',
-        class: 'heading heading--third-level',
-        text: 'Third level heading - only class'
-      }
-    },
-    {
-      name: 'class-4',
-      context: {
-        tag: 'h4',
-        class: 'heading heading--fourth-level',
-        text: 'Fourth level heading - only class'
-      }
-    },
-    {
-      name: 'class-5',
-      context: {
-        tag: 'h5',
-        class: 'heading heading--fifth-level',
-        text: 'Fifth level heading - only class'
-      }
-    },
-    {
-      name: 'class-6',
-      context: {
-        tag: 'h6',
-        class: 'heading heading--sixth-level',
-        text: 'Sixth level heading - only class'
       }
     },
     {
@@ -107,11 +27,43 @@ module.exports = {
       }
     },
     {
+      name: 'tag-2',
+      context: {
+        tag: 'h2',
+        class: '',
+        text: 'Second level heading - only tag'
+      }
+    },
+    {
+      name: 'class-2',
+      context: {
+        tag: 'h2',
+        class: 'heading heading--second-level',
+        text: 'Second level heading - only class'
+      }
+    },
+    {
       name: 'tag-and-class-2',
       context: {
         tag: 'div',
         class: 'heading heading--second-level',
         text: 'Second level heading - tag and class'
+      }
+    },
+    {
+      name: 'tag-3',
+      context: {
+        tag: 'h3',
+        class: '',
+        text: 'Third level heading - only tag'
+      }
+    },
+    {
+      name: 'class-3',
+      context: {
+        tag: 'h3',
+        class: 'heading heading--third-level',
+        text: 'Third level heading - only class'
       }
     },
     {
@@ -123,6 +75,22 @@ module.exports = {
       }
     },
     {
+      name: 'tag-4',
+      context: {
+        tag: 'h4',
+        class: '',
+        text: 'Fourth level heading - only tag'
+      }
+    },
+    {
+      name: 'class-4',
+      context: {
+        tag: 'h4',
+        class: 'heading heading--fourth-level',
+        text: 'Fourth level heading - only class'
+      }
+    },
+    {
       name: 'tag-and-class-4',
       context: {
         tag: 'div',
@@ -131,11 +99,43 @@ module.exports = {
       }
     },
     {
+      name: 'tag-5',
+      context: {
+        tag: 'h5',
+        class: '',
+        text: 'Fifth level heading - only tag'
+      }
+    },
+    {
+      name: 'class-5',
+      context: {
+        tag: 'h5',
+        class: 'heading heading--fifth-level',
+        text: 'Fifth level heading - only class'
+      }
+    },
+    {
       name: 'tag-and-class-5',
       context: {
         tag: 'div',
         class: 'heading heading--fifth-level',
         text: 'Fifth level heading - tag and class'
+      }
+    },
+    {
+      name: 'tag-6',
+      context: {
+        tag: 'h6',
+        class: '',
+        text: 'Sixth level heading - only tag'
+      }
+    },
+    {
+      name: 'class-6',
+      context: {
+        tag: 'h6',
+        class: 'heading heading--sixth-level',
+        text: 'Sixth level heading - only class'
       }
     },
     {
