78ec135f50b0ad946bf91975279772c4758be861
Filip Kudła <filip.kudla@snow.dog>
2017-06-09 14:43:03 +0200

    #30647 - Improve image element and add picture variant

--- components/02-elements/image/image.config.js
+++ components/02-elements/image/image.config.js
@@ -1,5 +1,24 @@
 module.exports = {
   context: {
-    url: '/images/banner/banner-480_480.png'
+    url: '/images/banner/banner-480_480.png',
+    sources: [
+      {
+        url: '/images/banner/banner-480_480.png',
+        mediaQuery: '(max-width: 480px)'
+      },
+      {
+        url: '/images/banner/banner-768_402.png',
+        mediaQuery: '(max-width: 768px)'
+      },
+      {
+        url: '/images/banner/banner-992_254.png',
+        mediaQuery: '(max-width: 992px)'
+      },
+      {
+        url: '/images/banner/banner-1170_300.png',
+        mediaQuery: ''
+      }
+    ],
+    defaultSrc: '/images/banner/banner-480_480.png',
   }
 };
