d24e14b4a358990387ea02edd859cc3882404bde
Bartek Igielski <igloczek@gmail.com>
2017-06-09 20:16:07 +0200

    Button component refactor

--- components/02-elements/button/button.config.js
+++ components/02-elements/button/button.config.js
@@ -1,85 +1,34 @@
 module.exports = {
-  status: 'prototype',
   collated: true,
   context: {
+    tag: 'button',
+    class: '',
+    attributes: 'type="button"',
     text: 'I am a button'
   },
   variants: [
     {
-      name: 'light',
-      context: {
-        text: 'Light button',
-        modifier: 'light',
-        iconId: 'arrow-right',
-        iconClass: 'button__icon'
-      }
-    },
-    {
       name: 'icon',
       context: {
-        text: 'I am a button',
-        modifier: 'icon',
+        text: '',
+        class: 'button--icon',
         iconId: 'arrow-right',
         iconClass: 'button__icon'
       }
     },
     {
-      name: 'capital',
+      name: 'secondary',
       context: {
-        text: 'I am a button',
-        modifier: 'capital',
-        iconId: 'arrow-right',
-        iconClass: 'button__icon'
-      }
-    },
-    {
-      name: 'color-icon',
-      context: {
-        text: 'I am a button',
-        modifier: 'color-icon',
-        iconId: 'arrow-right',
-        iconClass: 'button__icon'
-      }
-    },
-    {
-      name: 'dark',
-      context: {
-        text: 'I am a button',
-        modifier: 'dark',
-        iconId: 'arrow-right',
-        iconClass: 'button__icon'
-      }
-    },
-    {
-      name: 'borders',
-      context: {
-        text: 'I am a button',
-        modifier: 'borders',
-        modifier2: 'blue-icon',
-        iconId: 'arrow-right',
-        iconClass: 'button__icon'
+        class: 'button--secondary'
       }
     },
     {
       name: 'link',
       context: {
-        text: 'I am a link',
-        asLink: true
-      }
-    },
-    {
-      name: 'link blue',
-      context: {
-        asLink: true,
-        modifier: 'blue',
-        text: 'I am a link blue'
-      }
-    },
-    {
-      name: 'search',
-      context: {
-        text: 'I am a button',
-        modifier: 'search'
+        tag: 'a',
+        class: 'button--link',
+        attributes: 'href="#"',
+        text: 'I am a link button',
       }
     }
   ]
