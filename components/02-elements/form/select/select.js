31398511aab0ae24b153dff1ea7dc5c98ff6cadd
Bartek Igielski <igloczek@gmail.com>
2017-07-28 13:28:52 +0200

    #31479 - Unnecessary `args` variable removed

--- components/02-elements/form/select/select.js
+++ components/02-elements/form/select/select.js
@@ -3,19 +3,18 @@
 (function() { // eslint-disable-line
   'use strict'
 
-  const args    = {},
-        selects = [...document.querySelectorAll('.select__field')];
+  const selects = [...document.querySelectorAll('.select__field')];
 
   selects.forEach(select => {
-    args.searchEnabled = false;
-    args.classNames = {
-      containerInner: 'select__field',
-      list: 'select__field-list',
-      input: 'select__field-input',
-      item: 'select__field-item',
-      listSingle: 'select__field-list--single'
-    };
-
-    const choice = new Choices(select, args); // eslint-disable-line
+    new Choices(select, {
+      searchEnabled: false,
+      classNames: {
+        containerInner: 'select__field',
+        list: 'select__field-list',
+        input: 'select__field-input',
+        item: 'select__field-item',
+        listSingle: 'select__field-list--single'
+      }
+    });
   });
 }());
