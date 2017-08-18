2d0ce15402861fa0f58747e818d302c6fa748cb7
Filip Kudła <filip.kudla@snow.dog>
2017-08-18 17:15:42 +0200

    #31973 - Add dashboard new elements and replace toolbar with actions

---
+++ components/03-modules/dashboard-list-item/dashboard-list-item.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  context: {
+    title: 'Recently Ordered',
+    items: {
+      class: 'dashboard-list-item__list',
+      listTag: 'ol',
+      elementTag: 'li',
+      elements: [
+        {
+          text: 'First item',
+        },
+        {
+          text: 'Second item',
+        }
+      ]
+    },
+    buttons: {
+      links: [
+        {
+          title: 'Add to Cart'
+        },
+        {
+          title: 'View All'
+        }
+      ]
+    }
+  }
+}
