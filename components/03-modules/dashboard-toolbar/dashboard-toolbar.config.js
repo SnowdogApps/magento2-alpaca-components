cb6651d87354b1866c3e540a95ea39af4611ed99
Filip Kudła <filip.kudla@snow.dog>
2017-08-21 13:16:56 +0200

    #31973 - Update pager and add necessary things to dashboard-store-credit view

--- components/03-modules/dashboard-toolbar/dashboard-toolbar.config.js
+++ components/03-modules/dashboard-toolbar/dashboard-toolbar.config.js
@@ -1,5 +1,40 @@
 module.exports = {
   context: {
-    
+    leftItems: 'Items 1 to 10 of XX total',
+    pager: {
+      class: 'dashboard-toolbar__pager grid__column',
+      arrowLeftVisible: false,
+      arrowRightVisible: true,
+      pages: [
+        {
+          pageNo: 1,
+          active: false
+        },
+        {
+          pageNo: 2,
+          active: false
+        },
+        {
+          pageNo: 3,
+          active: false
+        }
+      ]
+    },
+    limiter: {
+      options: [
+        {
+          value: '10',
+          text: '10'
+        },
+        {
+          value: '20',
+          text: '20'
+        },
+        {
+          value: '50',
+          text: '50',
+        }
+      ]
+    }
   }
 }
--- components/04-views/dashboard/dashboard.config.js
+++ components/04-views/dashboard/dashboard.config.js
@@ -327,6 +327,45 @@ module.exports = {
           }
         },
       }
+    },
+    {
+      name: 'store-credit',
+      context: {
+        nav: {
+          title: 'My Account',
+          list: {
+            class: 'dashboard-nav__content',
+            listTag: 'ul',
+            elementTag: 'li',
+            elements: [
+              {
+                text: 'Account Dashboard',
+                class: 'dashboard-nav__item'
+              },
+              {
+                text: 'Account Information',
+                class: 'dashboard-nav__item'
+              },
+              {
+                text: 'Address Book',
+                class: 'dashboard-nav__item'
+              },
+              {
+                text: 'My Orders',
+                class: 'dashboard-nam__item '
+              },
+              {
+                text: 'Store Credit',
+                class: 'dashboard-nav__item current'
+              },
+              {
+                text: 'My Downloadable Products',
+                class: 'dashboard-nav__item'
+              }
+            ]
+          }
+        },
+      }
     }
   ]
 }
