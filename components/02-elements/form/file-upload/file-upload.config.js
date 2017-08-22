c55fe45eaaa9c46826d37de4b54d453d9e092c3d
Filip Kudła <filip.kudla@snow.dog>
2017-08-22 12:25:46 +0200

    #31973 - Add file-upload new form type

---
+++ components/02-elements/form/file-upload/file-upload.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  context: {
+    fileId: 'file',
+    labelText: 'Your file (pdf, doc, max 1 MB)',
+    labelTextSecond: 'Choose a file',
+    type: 'file',
+    name: 'uploadCv',
+    fileName: 'File not choosen',
+    text: 'Choose a file'
+  }
+}
