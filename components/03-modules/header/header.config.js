6af156dc208f18ec7724a89b368934db7d81fef5
Pawel Szafranski <szafran89@gmail.com>
2017-10-23 19:40:28 +0200

    #35070 - header - logo and mobile links styles

--- components/03-modules/header/header.config.js
+++ components/03-modules/header/header.config.js
@@ -1,13 +1,60 @@
 module.exports = {
+  title: 'Header module',
   context: {
+    logo: {
+      class: 'header__logo',
+      logoSVG: true
+    },
+    button: {
+      tag: 'button',
+      text: '',
+      class: 'button--icon header__search-button',
+      iconId: 'search',
+      iconClass: 'button__icon',
+      attributes: 'type="button"',
+    },
+    form: {
+      class: 'form--compact header__search-form',
+      input: {
+        class: 'header__input',
+        field: {
+          class: 'header__field',
+          placeholder: 'Search'
+        }
+      },
+      button: {
+        class: 'button--icon',
+        tag: 'a',
+        iconId: 'search',
+        iconClass: 'button__icon'
+      },
+    },
+    likeButton: {
+      tag: 'button',
+      text: '',
+      class: 'button--icon header__button header__button--like',
+      iconId: 'like',
+      iconClass: 'button__icon'
+    },
+    infoButton: {
+      tag: 'button',
+      text: '',
+      class: 'button--icon header__button header__button--info',
+      iconId: 'info',
+      iconClass: 'button__icon'
+    },
     trigger: {
       attributes: 'type="button"',
       class: 'button popup__button',
-      counter: '99'
+      counter: '82'
     },
     minicartPopup: {
-      script: true,
+      script: false,
       content: 'minicart-content'
+    },
+    loginPopup: {
+      script: true,
+      content: 'login-form--popup'
     }
   }
 }
---
+++ components/03-modules/header/header.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const mediaQuery    = window.matchMedia('(min-width: 768px)'),
+      searchWrapper = document.querySelector('.header__search-wrapper'),
+      button        = searchWrapper.querySelector('.header__search-button'),
+      searchForm    = searchWrapper.querySelector('.header__search-form');
+
+if (!mediaQuery.matches) {
+  button.addEventListener('click', smallFormListener);
+}
+
+window.addEventListener('resize', () => {
+  if (!mediaQuery.matches) {
+    button.addEventListener('click', smallFormListener);
+  }
+  else {
+    if (searchForm.classList.contains('header__search-form--visible')) {
+      searchForm.classList.remove('header__search-form--visible');
+    }
+
+    button.removeEventListener('click', smallFormListener);
+  }
+});
+
+function smallFormListener() {
+  searchForm.classList.toggle('header__search-form--visible');
+}
