3f39aad5f42b4da07712abb811a87d1a92cfb4cc
Filip Kudła <filip.kudla@snow.dog>
2017-10-24 12:56:54 +0200

    #35132 - Refactor banner code, simplify template, add image to config

--- components/02-elements/banner/banner.config.js
+++ components/02-elements/banner/banner.config.js
@@ -1,12 +1,25 @@
 module.exports = {
   context: {
-    heading: 'Lorem ipsum dolor',
-    image: '@image--picture',
-    button: {
-      class: 'button--dark',
-      text: 'I am a button',
-      tag: 'button',
-      attributes: 'type="button"'
+    image: {
+      defaultSrc: '/images/banner/banner-480_480.png',
+      sources: [
+        {
+          src: '/images/banner/banner-480_480.png',
+          mediaQuery: '(max-width: 480px)'
+        },
+        {
+          src: '/images/banner/banner-768_402.png',
+          mediaQuery: '(max-width: 768px)'
+        },
+        {
+          src: '/images/banner/banner-992_254.png',
+          mediaQuery: '(max-width: 992px)'
+        },
+        {
+          src: '/images/banner/banner-1170_300.png',
+          mediaQuery: ''
+        }
+      ]
     }
   }
 };
