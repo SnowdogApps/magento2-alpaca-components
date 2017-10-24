e368947b651d9984331533d88f02ce0d3d1e716b
Ania Karon <anna.karon@snow.dog>
2017-10-24 13:39:17 +0200

    #36109 dropdown-list refactor

--- components/02-elements/dropdown-list/dropdown-list.config.js
+++ components/02-elements/dropdown-list/dropdown-list.config.js
@@ -3,169 +3,52 @@ module.exports = {
   context: {
     dropdowns: [
       {
+        itemTag: 'a',
         title: 'All',
-        link: '#'
+        itemAttributes: 'href="#"',
+        collapse: false
       },
       {
+        itemTag: 'button',
         title: 'dropdown title',
         id: 'dropdown-1',
-        list: [
-          {
-            itemName: 'Item name',
-            itemLink: '#',
-            id: 'dropdown-2',
-            sublist: [
-              {
-                itemName: 'sublist item name',
-                itemLink: '#'
-              },
-              {
-                itemName: 'sublist item name',
-                itemLink: '#'
-              },
-              {
-                itemName: 'sublist item name',
-                itemLink: '#'
-              },
-              {
-                itemName: 'sublist item name',
-                itemLink: '#'
-              }
-            ]
-          },
-          {
-            itemName: 'Item name',
-            itemLink: '#',
-            id: 'dropdown-3',
-            sublist: [
-              {
-                itemName: 'sublist item name',
-                itemLink: '#'
-              },
-              {
-                itemName: 'sublist item name',
-                itemLink: '#'
-              },
-              {
-                itemName: 'sublist item name',
-                itemLink: '#'
-              }
-            ]
-          },
-          {
-            itemName: 'Item name',
-            itemLink: '#',
-            sublist: false
-          },
-          {
-            itemName: 'Item name',
-            itemLink: '#',
-            sublist: false
-          }
-        ]
+        collapse: true,
+        contentElement: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce posuere magna at erat mollis egestas a ut quam. Pellentesque ac varius ligula.</p>'
       },
       {
+        itemTag: 'button',
+        title: 'some longer longer and longer dropdown title',
+        id: 'dropdown-2',
+        collapse: true,
+        contentElement: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce posuere magna at erat mollis egestas a ut quam. Pellentesque ac varius ligula.</p>'
+      },
+      {
+        itemTag: 'button',
+        title: 'dropdown title',
+        id: 'dropdown-3',
+        collapse: true,
+        contentElement: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce posuere magna at erat mollis egestas a ut quam. Pellentesque ac varius ligula.</p>'
+      },
+      {
+        itemTag: 'button',
         title: 'dropdown title',
         id: 'dropdown-4',
-        list: [
-          {
-            itemName: 'Item name',
-            itemLink: '#',
-            sublist: false
-          },
-          {
-            itemName: 'Item name',
-            itemLink: '#',
-            sublist: false
-          }
-        ]
-      },
-      {
-        title: 'dropdown title',
-        id: 'dropdown-5',
-        list: [
-          {
-            itemName: 'Item name',
-            itemLink: '#',
-            id: 'dropdown-6',
-            sublist: [
-              {
-                itemName: 'sublist item name',
-                itemLink: '#'
-              },
-              {
-                itemName: 'sublist item name',
-                itemLink: '#'
-              }
-            ]
-          },
-          {
-            itemName: 'Item name',
-            itemLink: '#',
-            sublist: false
-          },
-          {
-            itemName: 'Item name',
-            itemLink: '#',
-            id: 'dropdown-7',
-            sublist: [
-              {
-                itemName: 'sublist item name',
-                itemLink: '#'
-              },
-              {
-                itemName: 'sublist item name',
-                itemLink: '#'
-              },
-              {
-                itemName: 'sublist item name',
-                itemLink: '#'
-              },
-              {
-                itemName: 'sublist item name',
-                itemLink: '#'
-              },
-              {
-                itemName: 'sublist item name',
-                itemLink: '#'
-              },
-              {
-                itemName: 'sublist item name',
-                itemLink: '#'
-              }
-            ]
-          },
-          {
-            itemName: 'Item name',
-            itemLink: '#',
-            id: 'dropdown-8',
-            sublist: [
-              {
-                itemName: 'sublist item name',
-                itemLink: '#'
-              }
-            ]
-          },
-          {
-            itemName: 'item name',
-            itemLink: '#',
-            id: 'dropdown-9',
-            sublist: [
-              {
-                itemName: 'sublist item name',
-                itemLink: '#'
-              }
-            ]
-          }
-        ]
+        collapse: true,
+        contentElement: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce posuere magna at erat mollis egestas a ut quam. Pellentesque ac varius ligula.</p>'
       }
     ]
   },
   variants: [
     {
-      name: 'item-wide',
+      name: 'with-nested',
       context: {
-        class: 'dropdown-list--item-wide'
+        class: 'dropdown-list--with-nested'
+      }
+    },
+    {
+      name: 'middle-screen-inline',
+      context: {
+        class: 'dropdown-list--middle-inline'
       }
     }
   ]
--- components/02-elements/dropdown-list/dropdown-list.js
+++ components/02-elements/dropdown-list/dropdown-list.js
@@ -1,7 +1,6 @@
 'use-strict'
-const currentInnerItem  = document.querySelector('.dropdown-list__inner-link.current'),
-      dropdownItem      = document.querySelectorAll('.dropdown-list__item--parent .dropdown-list__link'),
-      dropdownInnerItem = document.querySelectorAll('.dropdown-list__inner-item--parent .dropdown-list__inner-link');
+const dropdownItem = [ ...document.querySelectorAll('.dropdown-list__item--collapse .dropdown-list__label') ],
+      openClass = 'dropdown-list__item--open';
 
 function setListHeight(item) {
   return Array.from(item.children)
@@ -9,71 +8,24 @@ function setListHeight(item) {
     .reduce((a, b) => a + b, 0);
 }
 
-function toggleSubmenu(item, inner) {
-  const dropdownId = item.dataset.dropdown;
-  let dropdownList = document.querySelector(`.dropdown-list__inner-list--level1[data-dropdown="${dropdownId}"]`),
-      innerLists   = item.parentNode.querySelectorAll('.dropdown-list__inner-list--level2'),
-      upperList    = null;
+function toggleContent(item) {
+  const dropdownId = item.dataset.dropdown,
+        dropdownItem = item.parentNode,
+        dropdownContent = dropdownItem.querySelector(`.dropdown-list__content[data-content="${dropdownId}"]`);
 
-  if (inner) {
-    dropdownList = item.parentNode.querySelector(`.dropdown-list__inner-list--level2[data-dropdown="${dropdownId}"]`);
-    innerLists = null;
-    upperList  = item.closest('.dropdown-list__inner-list--level1');
-  }
-
-  item.parentNode.classList.toggle('open');
-
-  if (dropdownList && dropdownList.clientHeight > 0) {
-    if (innerLists) {
-      innerLists.forEach(key => {
-        key.style.height = 0;
-        key.parentNode.classList.remove('open');
-      });
-    }
-    else if (upperList) {
-      upperList.style.height = upperList.clientHeight - dropdownList.clientHeight + 'px';
-    }
-
-    dropdownList.style.height = 0;
+  if (dropdownContent.clientHeight > 0) {
+    dropdownContent.style.height = 0;
+    dropdownItem.classList.remove(openClass);
   }
   else {
-    const listHeight = setListHeight(dropdownList);
-    dropdownList.style.height = listHeight + 'px';
-
-    if (upperList) {
-      upperList.style.height = upperList.clientHeight + listHeight + 'px';
-    }
-  }
-}
-
-if (currentInnerItem !== null) {
-  const currentList       = currentInnerItem.closest('.filters__inner-list'),
-        currentListHeight = setListHeight(currentList);
-  let currentUpperList = null;
-
-  if (currentList.classList.contains('filters__inner-list--level2')) {
-    currentUpperList = currentList.closest('.filters__inner-list--level1');
-  }
-
-  currentList.style.height = currentListHeight + 'px';
-  currentList.parentNode.classList.toggle('open');
-
-  if (currentUpperList) {
-    currentUpperList.style.height = currentUpperList.clientHeight + setListHeight(currentUpperList) + currentListHeight + 'px';
-    currentUpperList.parentNode.classList.toggle('open');
+    dropdownContent.style.height = `${setListHeight(dropdownContent)}px`;
+    dropdownItem.classList.add(openClass);
   }
 }
 
 dropdownItem.forEach(
   key => key.addEventListener('click', (e) => {
     e.preventDefault();
-    toggleSubmenu(key, false);
-  }, false)
-);
-
-dropdownInnerItem.forEach(
-  key => key.addEventListener('click', (e) => {
-    e.preventDefault();
-    toggleSubmenu(key, true);
+    toggleContent(key, false);
   }, false)
 );
