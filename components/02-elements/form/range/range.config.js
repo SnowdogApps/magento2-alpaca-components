5b4fafb11309480d295124a3672c140fb431c963
Janusz Janczy <janusz.janczy@snow.dog>
2017-10-30 15:46:37 +0100

    #35069: add noUiSlider (WIP)

---
+++ components/02-elements/form/range/range.config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  status: 'wip',
+  preview: '@docs-only-styles',
+  context: {
+    class: '',
+    button: {
+      label: 'Filter'
+    },
+    'filter': {
+      wrapperClass: '',
+      input: {
+        'upperClass': '',
+        'lowerClass': '',
+        'maxValue': '1000',
+        'minValue': '0',
+        'type': 'text'
+      }
+    },
+    'rangeInputLower': {
+      'class': 'range-filter__input range-filter__input--lower',
+      'label': {
+        'class': 'range-filter__label',
+        'text': 'Minimum value'
+      },
+      'field': {
+        'class': 'range-filter__field range-filter__field--lower',
+        'placeholder': '',
+        'value': '0'
+      }
+    },
+    'rangeInputUpper': {
+      'class': 'range-filter_input range-filter__input--upper',
+      'label': {
+        'class': 'range-filter__label',
+        'text': 'Maximum value'
+      },
+      'field': {
+        'class': 'range-filter__field range-filter__field--upper',
+        'placeholder': '',
+        'value': '1000'
+      }
+    }
+  },
+  variants: [
+    {
+      name: 'inline',
+      context: {
+        class: 'input--inline'
+      }
+    }
+  ]
+};
---
+++ components/02-elements/form/range/range.js
@@ -0,0 +1,67 @@
+/* global noUiSlider */
+(function () { // eslint-disable-line
+  'use strict';
+
+  function init() {
+    const rangeFilters = [...document.querySelectorAll('.range-filter')];
+    rangeFilters.forEach(rangeFilter => {
+      createRangeSlider(rangeFilter);
+    });
+  }
+
+  function createRangeSlider(rangeFilter) {
+
+    const rangeSlider = rangeFilter.querySelector('.range-filter__slider'),
+          inputMax = rangeFilter.querySelector('.range-filter__upper'),
+          inputMin = rangeFilter.querySelector('.range-filter__lower');
+
+    initNoUiSlider(rangeSlider);
+    bindEvents(rangeSlider, inputMin, inputMax);
+
+  }
+
+  function initNoUiSlider(rangeSlider) {
+
+    noUiSlider.create(rangeSlider, {
+      start: [0, 1000],
+      connect: true,
+      tooltips: [
+        true,
+        true
+      ],
+      range: {
+        'min': 0,
+        'max': 200
+      },
+      formatter: {
+        to(value) {
+          return value + ',-';
+        },
+        from(value) {
+          return value.replace(',-', '');
+        }
+      },
+    });
+
+  }
+
+  function bindEvents(rangeSlider, inputMin, inputMax) {
+
+    rangeSlider.noUiSlider.on('update', (values) => {
+      inputMin.value = values[0];
+      inputMax.value = values[1];
+    });
+
+    inputMax.addEventListener('change', () => {
+      rangeSlider.noUiSlider.set([null, this.value]);
+    });
+
+    inputMin.addEventListener('change', () => {
+      rangeSlider.noUiSlider.set([null, this.value]);
+    });
+  }
+
+  init();
+
+
+}());
