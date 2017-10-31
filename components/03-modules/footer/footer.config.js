d3534a1e4395d202bdfd546c951c743991a588cd
Ania Karon <anna.karon@snow.dog>
2017-10-31 16:56:20 +0100

    #35474 footer module - bottom bar created

---
+++ components/03-modules/footer/footer.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  context: {
+    socialListSubtitle: 'Find us on',
+    copyrightText: 'Copyright © 2017 Alpaca. All rights reserved.',
+    socialIconList: {
+      listTag: 'ul',
+      elementTag: 'li',
+      class: 'list--with-icon list--horizontal footer__social-list',
+      elements: [
+        {
+          icon: 'youtube',
+          class: 'footer__social-list-item',
+          iconClass: 'footer__social-icon'
+        },
+        {
+          icon: 'facebook',
+          class: 'footer__social-list-item',
+          iconClass: 'footer__social-icon'
+        },
+        {
+          icon: 'instagram',
+          class: 'footer__social-list-item',
+          iconClass: 'footer__social-icon'
+        },
+        {
+          icon: 'google-plus',
+          class: 'footer__social-list-item',
+          iconClass: 'footer__social-icon'
+        }
+      ]
+    }
+  }
+}
