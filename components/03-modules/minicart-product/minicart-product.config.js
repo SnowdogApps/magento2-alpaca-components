27ab1faf5729d0b8d07be04137279658f956c25f
Ania Karon <anna.karon@snow.dog>
2017-11-07 00:40:09 +0100

    #35672 add minicart-product module

---
+++ components/03-modules/minicart-product/minicart-product.config.js
@@ -0,0 +1,53 @@
+module.exports = {
+  context: {
+    productImage: {
+      src: 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==',
+      dataSrc: '/images/product/minicart-product-72_72.png',
+      alt: 'product name goes here'
+    },
+    productName: 'Some product name',
+    productPrice: '$85.00',
+    qtyInput: {
+      class: 'input--inline minicart-product__qty-input',
+      label: {
+        attribute: '',
+        text: 'Qty',
+      },
+      field: {
+        attribute: 'type="number" value="1"',
+        class: '',
+        placeholder: 'Qty'
+      }
+    },
+    editIcon: {
+      tag: 'button',
+      attributes: 'type="button" aria-label="edit product in your shopping cart"',
+      text: '',
+      class: 'button--icon minicart-product__button',
+      iconClass: 'button__icon',
+      iconId: 'edit',
+    },
+    removeIcon: {
+      tag: 'button',
+      attributes: 'type="button" aria-label="remove this product from your shopping cart"',
+      text: '',
+      class: 'button--icon minicart-product__button',
+      iconClass: 'button__icon',
+      iconId: 'remove',
+    },
+    configurable: true,
+    detailsList: {
+      class: 'minicart-product__details',
+      dropdowns: [
+        {
+          itemTag: 'button',
+          itemAttributes: 'aria-expanded="false" aria-controls="minicart-product-details-1"',
+          title: 'see product details',
+          id: 'minicart-product-details-1',
+          collapse: true,
+          contentElement: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce posuere magna at erat mollis egestas a ut quam. Pellentesque ac varius ligula.</p>'
+        },
+      ]
+    }
+  }
+}
