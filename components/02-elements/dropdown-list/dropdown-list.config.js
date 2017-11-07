5f74c8252d220bd733d2617c5ab0a3ff6defbca3
Filip Kudła <filip.kudla@snow.dog>
2017-11-07 13:49:22 +0100

    #35474 - Rename class to BEMIT

--- components/02-elements/dropdown-list/dropdown-list.config.js
+++ components/02-elements/dropdown-list/dropdown-list.config.js
@@ -46,7 +46,7 @@ module.exports = {
     {
       name: 'screen-m',
       context: {
-        class: 'dropdown-list@screen-m js-is-open-screen-m',
+        class: 'dropdown-list@screen-m dropdown-list--is-open@screen-m',
       }
     },
     {
