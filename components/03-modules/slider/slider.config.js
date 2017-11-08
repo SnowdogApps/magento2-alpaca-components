a126fc3dd8c00964cff33834681dfd67d52c5b70
Filip Kudła <filip.kudla@snow.dog>
2017-11-08 15:25:24 +0100

    #35432 - Add self created content to slider as item, edit config, add styles

--- components/03-modules/slider/slider.config.js
+++ components/03-modules/slider/slider.config.js
@@ -5,63 +5,57 @@ module.exports = {
     iconClass: 'slider__icon',
     products: [
       {
-        product: {
-          image: {
-            src: '/images/product/product-base.jpg',
-          },
-          class: 'slider__item',
-          tag: 'div',
-          name: 'Some product name',
-          price: '$ 200.00',
+        image: {
+          class: 'slider__image',
+          dataSrc: '/images/product/product-base.jpg',
+        },
+        name: 'Some product name',
+        price: '$ 200.00',
+        oldPrice: '$ 250.00'
+      },
+      {
+        image: {
+          class: 'slider__image',
+          src: '/images/product/product-second.jpg',
+        },
+        name: 'Some product name',
+        price: '$ 200.00',
+        oldPrice: '$ 250.00',
+        badge: {
+          text: 'New',
+          class: 'badge--new slider__badge'
         }
       },
       {
-        product: {
-          image: {
-            src: '/images/product/product-second.jpg',
-          },
-          class: 'slider__item',
-          tag: 'div',
-          name: 'Some product name',
-          price: '$ 200.00',
-          badge: {
-            text: 'New',
-            class: 'badge--new'
-          }
+        image: {
+          class: 'slider__image',
+          dataSrc: '/images/product/product-base.jpg',
+        },
+        name: 'Some product name',
+        price: '$ 200.00',
+        oldPrice: '$ 250.00'
+      },
+      {
+        image: {
+          class: 'slider__image',
+          src: '/images/product/product-second.jpg',
+        },
+        name: 'Some product name',
+        price: '$ 200.00',
+        oldPrice: '$ 250.00',
+        badge: {
+          text: '-20%',
+          class: 'badge--discount slider__badge'
         }
       },
       {
-        product: {
-          image: {
-            src: '/images/product/product-base.jpg',
-          },
-          class: 'slider__item',
-          tag: 'div',
-          name: 'Some product name',
-          price: '$ 200.00',
-        }
-      },
-      {
-        product: {
-          image: {
-            src: '/images/product/product-second.jpg',
-          },
-          class: 'slider__item',
-          tag: 'div',
-          name: 'Some product name',
-          price: '$ 200.00',
-        }
-      },
-      {
-        product: {
-          image: {
-            src: '/images/product/product-base.jpg',
-          },
-          class: 'slider__item',
-          tag: 'div',
-          name: 'Some product name',
-          price: '$ 200.00',
-        }
+        image: {
+          class: 'slider__image',
+          dataSrc: '/images/product/product-base.jpg',
+        },
+        name: 'Some product name',
+        price: '$ 200.00',
+        oldPrice: '$ 250.00'
       }
     ]
   }
