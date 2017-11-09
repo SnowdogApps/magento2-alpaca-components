1285a285d1be6b11b885ddebf65af699a8eafa16
Ania Karon <anna.karon@snow.dog>
2017-11-09 13:16:05 +0100

    #35743 update icons preview with icon component usage

--- components/01-globals/icons/icons.config.js
+++ components/01-globals/icons/icons.config.js
@@ -2,28 +2,94 @@ module.exports = {
   preview: '@docs-only-styles',
   context: {
     icons: [
-      'angle-down',
-      'arrow-circle-up',
-      'arrow-left',
-      'checked',
-      'close',
-      'youtube',
-      'facebook',
-      'instagram',
-      'google-plus',
-      'mobile-menu',
-      'minus',
-      'search',
-      'phone',
-      'user',
-      'shopping-cart',
-      'settings',
-      'edit',
-      'linkedin',
-      'loader',
-      'plus',
-      'remove',
-      'twitter'
+      {
+        iconId: 'angle-down',
+        class: 'icons__single-icon'
+      },
+      {
+        iconId: 'arrow-circle-up',
+        class: 'icons__single-icon'
+      },
+      {
+        iconId: 'arrow-left',
+        class: 'icons__single-icon'
+      },
+      {
+        iconId: 'checked',
+        class: 'icons__single-icon'
+      },
+      {
+        iconId: 'close',
+        class: 'icons__single-icon'
+      },
+      {
+        iconId: 'youtube',
+        class: 'icons__single-icon'
+      },
+      {
+        iconId: 'facebook',
+        class: 'icons__single-icon'
+      },
+      {
+        iconId: 'instagram',
+        class: 'icons__single-icon'
+      },
+      {
+        iconId: 'google-plus',
+        class: 'icons__single-icon'
+      },
+      {
+        iconId: 'mobile-menu',
+        class: 'icons__single-icon'
+      },
+      {
+        iconId: 'minus',
+        class: 'icons__single-icon'
+      },
+      {
+        iconId: 'search',
+        class: 'icons__single-icon'
+      },
+      {
+        iconId: 'phone',
+        class: 'icons__single-icon'
+      },
+      {
+        iconId: 'user',
+        class: 'icons__single-icon'
+      },
+      {
+        iconId: 'shopping-cart',
+        class: 'icons__single-icon'
+      },
+      {
+        iconId: 'settings',
+        class: 'icons__single-icon'
+      },
+      {
+        iconId: 'edit',
+        class: 'icons__single-icon'
+      },
+      {
+        iconId: 'linkedin',
+        class: 'icons__single-icon'
+      },
+      {
+        iconId: 'loader',
+        class: 'icons__single-icon'
+      },
+      {
+        iconId: 'plus',
+        class: 'icons__single-icon'
+      },
+      {
+        iconId: 'remove',
+        class: 'icons__single-icon'
+      },
+      {
+        iconId: 'twitter',
+        class: 'icons__single-icon'
+      }
     ]
   }
 };
