274fc63144b0af272884503361712ea631dbb4c3
Piotr Krusicki <piotr.krusicki@gmail.com>
2017-11-09 11:48:53 +0100

    #35702 Cookie message: Default variant example text changed.

--- components/02-elements/cookie-message/cookie-message.config.js
+++ components/02-elements/cookie-message/cookie-message.config.js
@@ -4,7 +4,7 @@ module.exports = {
     linkAnchor: 'Action\'s details',
     closeIcon: 'close',
     closeLabel: false,
-    text: 'Some action / promotion text',
+    text: '<a href="#">Example link</a> Find out more about their purpose and settings in your browser. By browsing the site you are agreeing to use cookies according to your browser settings.',
     class: 'cookie-message',
     dataType: 'cookie'
   },
@@ -15,7 +15,7 @@ module.exports = {
         linkAnchor: 'Details',
         closeIcon: 'close',
         closeLabel: 'Close',
-        text: 'This site uses cookies. <a href="#"> test</a> Find out more about their purpose and settings in your browser. By browsing the site you are agreeing to use cookies according to your browser settings.',
+        text: 'This site uses cookies. <a href="#">Example link</a> Find out more about their purpose and settings in your browser. By browsing the site you are agreeing to use cookies according to your browser settings.',
         class: 'cookie-message--with-button',
         dataType: 'cookie1'
       }
