d01760f45e5e44d350b556ee6dc19435be52bd32
Piotr Krusicki <piotr.krusicki@gmail.com>
2017-11-09 11:40:17 +0100

    #35702 Cookie message: styling that fits new alpaca design.

--- components/02-elements/cookie-message/cookie-message.config.js
+++ components/02-elements/cookie-message/cookie-message.config.js
@@ -3,18 +3,21 @@ module.exports = {
   context: {
     linkAnchor: 'Action\'s details',
     closeIcon: 'close',
+    closeLabel: false,
     text: 'Some action / promotion text',
-    class: 'cookie-message--top'
+    class: 'cookie-message',
+    dataType: 'cookie'
   },
   variants: [
     {
       name: 'With button',
       context: {
         linkAnchor: 'Details',
-        closeIcon:  true,
-        closeLabel: 'Close  X',
-        text: 'This site uses cookies. Find out more about their purpose and settings in your browser. By browsing the site you are agreeing to use cookies according to your browser settings.',
-        class: 'cookie-message--with-button'
+        closeIcon: 'close',
+        closeLabel: 'Close',
+        text: 'This site uses cookies. <a href="#"> test</a> Find out more about their purpose and settings in your browser. By browsing the site you are agreeing to use cookies according to your browser settings.',
+        class: 'cookie-message--with-button',
+        dataType: 'cookie1'
       }
     }
   ]
