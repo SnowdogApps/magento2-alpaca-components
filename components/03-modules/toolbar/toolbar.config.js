064b57fb6bb33dbf6510878a440c5b2323141aa8
Janusz Janczy <janusz.janczy@snow.dog>
2017-11-10 23:32:57 +0100

    #35890: add category toolbar module

---
+++ components/03-modules/toolbar/toolbar.config.js
@@ -0,0 +1,63 @@
+module.exports = {
+  context: {
+    amount: {
+      prefix: 'Items',
+      currentRange: '1-9',
+      preposition: 'of',
+      total: '12'
+    },
+    sortBy: {
+      class: 'toolbar__sorter-wrapper',
+      label: {
+        attribute: '',
+        text: 'Sort By',
+        class: 'toolbar__sorter-label'
+      },
+      field: {
+        id: 'sort-by',
+        name: 'sort-by',
+        class: 'toolbar__sorter-select'
+      },
+      options: [
+        {
+          text: 'Product Name',
+          value: 'product-name'
+        },
+        {
+          text: 'Price',
+          value: 'price'
+        },
+      ]
+    },
+    limiter: {
+      select: {
+        class: 'toolbar__limiter-wrapper',
+        label: {
+          attribute: '',
+          text: 'Show',
+          class: 'toolbar__limiter-label'
+        },
+        field: {
+          id: 'sort-by',
+          name: 'sort-by',
+          class: 'toolbar__limiter-select'
+        },
+        options: [
+          {
+            text: '12',
+            value: '12'
+          },
+          {
+            text: '24',
+            value: '24'
+          },
+          {
+            text: '36',
+            value: '36'
+          },
+        ]
+      },
+      suffix: 'per page'
+    }
+  }
+}
