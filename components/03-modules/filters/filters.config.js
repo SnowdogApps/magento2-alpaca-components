dff29dd57dfe392c2eeddbe82cc9481b2c6c78dd
Piotr Krusicki <piotr.krusicki@gmail.com>
2017-11-13 16:13:38 +0100

    #35910 Filters module: Range initial hbs usage & config.js

---
+++ components/03-modules/filters/filters.config.js
@@ -0,0 +1,56 @@
+module.exports = {
+  title: 'Filters',
+  context: {
+    tag: 'div',
+    range: {
+      attributes: '',
+      class: '',
+      filter: {
+        wrapperClass: '',
+        input: {
+          upperClass: '',
+          lowerClass: '',
+          maxValue: '1000',
+          minValue: '0',
+          type: 'text'
+        },
+        suffix: {
+          lowerText: '$',
+          upperText: '$',
+          lowerClass: '',
+          upperClass: ''
+        }
+      },
+      rangeInputLower: {
+        class: 'range-filter__input range-filter__input--lower',
+        label: {
+          class: 'range-filter__label',
+          text: 'Minimum value'
+        },
+        field: {
+          class: 'range-filter__field range-filter__field--lower',
+          id: 'range-filter-min',
+          name: 'range-filter-min',
+          placeholder: '',
+          value: '0'
+        }
+      },
+      rangeInputUpper: {
+        class: 'range-filter__input range-filter__input--upper',
+        label: {
+          class: 'range-filter__label',
+          text: 'Maximum value'
+        },
+        field: {
+          class: 'range-filter__field range-filter__field--upper',
+          id: 'range-filter-max',
+          name: 'range-filter-max',
+          placeholder: '',
+          value: '1000'
+        }
+      },
+      script: true,
+      showFloat: 0
+    }
+  }
+};
