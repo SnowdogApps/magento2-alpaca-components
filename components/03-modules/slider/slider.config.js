4efc2f21cb89c9abdada145a97a512b971ae87e6
Filip Kudła <filip.kudla@snow.dog>
2017-11-14 12:53:25 +0100

    #36012 - Delete code with product instead of rendering done component, cleanup styles and config

--- components/03-modules/slider/slider.config.js
+++ components/03-modules/slider/slider.config.js
@@ -17,88 +17,9 @@ module.exports = {
       iconClass: 'button__icon slider__icon',
       attributes: 'type="button" aria-label="button arrow slider next"'
     },
-    products: [
+    elements: [
       {
-        link: '#',
-        ariaLabel: 'first product slider',
-        image: {
-          class: 'slider__image',
-          src: '/images/product/product-base.jpg',
-        },
-        heading: {
-          tag: 'h5',
-          class: 'slider__name',
-          text: 'Some product name',
-        },
-        price: '$ 200.00',
-        oldPrice: '$ 250.00'
-      },
-      {
-        link: '#',
-        ariaLabel: 'second product slider',
-        image: {
-          class: 'slider__image',
-          src: '/images/product/product-second.jpg',
-        },
-        heading: {
-          tag: 'h5',
-          class: 'slider__name',
-          text: 'Some product name',
-        },
-        price: '$ 200.00',
-        badge: {
-          text: 'New',
-          class: 'badge--new slider__badge'
-        }
-      },
-      {
-        link: '#',
-        ariaLabel: 'third product slider',
-        image: {
-          class: 'slider__image',
-          src: '/images/product/product-base.jpg',
-        },
-        heading: {
-          tag: 'h5',
-          class: 'slider__name',
-          text: 'Some product name',
-        },
-        price: '$ 200.00',
-        oldPrice: '$ 250.00'
-      },
-      {
-        link: '#',
-        ariaLabel: 'fourth product slider',
-        image: {
-          class: 'slider__image',
-          src: '/images/product/product-second.jpg',
-        },
-        heading: {
-          tag: 'h5',
-          class: 'slider__name',
-          text: 'Some product name',
-        },
-        price: '$ 200.00',
-        oldPrice: '$ 250.00',
-        badge: {
-          text: '-20%',
-          class: 'badge--discount slider__badge'
-        }
-      },
-      {
-        link: '#',
-        ariaLabel: 'fifth product slider',
-        image: {
-          class: 'slider__image',
-          src: '/images/product/product-base.jpg',
-        },
-        heading: {
-          tag: 'h5',
-          class: 'slider__name',
-          text: 'Some product name',
-        },
-        price: '$ 200.00',
-        oldPrice: '$ 250.00'
+        content: 'button'
       }
     ]
   }
