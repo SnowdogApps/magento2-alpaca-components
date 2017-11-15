bef7d23c05c6d664743a5975232db47c5fbd1cb5
Piotr Krusicki <piotr.krusicki@gmail.com>
2017-11-15 09:41:06 +0100

    #35910 Filters: Swatches(default,color) initial config & hbs usage.

--- components/03-modules/filters/filters.config.js
+++ components/03-modules/filters/filters.config.js
@@ -119,6 +119,66 @@ module.exports = {
           class: ''
         }
       }
-    ]
+    ],
+    swatch: {
+      class: '',
+      attributes: '',
+      titleClass: '',
+      title: 'Choose your size',
+      options: [
+        {
+          class: '',
+          attributes: '',
+          text: 'XS'
+        },
+        {
+          class: 'swatch__option--active',
+          attributes: '',
+          text: 'S'
+        },
+        {
+          class: '',
+          attributes: '',
+          text: 'M'
+        },
+        {
+          class: '',
+          attributes: '',
+          text: 'L'
+        },
+        {
+          class: '',
+          attributes: '',
+          text: 'XL'
+        }
+      ]
+    },
+    color: {
+      name: 'color',
+      class: '',
+      title: 'Choose color',
+      options: [
+        {
+          class: 'swatch__option--color',
+          attributes: 'style="background: #000"',
+          text: ''
+        },
+        {
+          class: 'swatch__option--color',
+          attributes: 'style="background: #fff"',
+          text: ''
+        },
+        {
+          class: 'swatch__option--color swatch__option--active',
+          attributes: 'style="background: #5cb85c"',
+          text: ''
+        },
+        {
+          class: 'swatch__option--color',
+          attributes: 'style="background: #0275d8"',
+          text: ''
+        }
+      ]
+    }
   }
 };
