063c446b3ee322436950bc67170685c55b87a469
Piotr Krusicki <piotr.krusicki@gmail.com>
2017-11-16 14:28:58 +0100

    #35910 Filters:  Using new 'filter' component in config & hbs.

--- components/03-modules/filters/filters.config.js
+++ components/03-modules/filters/filters.config.js
@@ -4,7 +4,7 @@ module.exports = {
     tag: 'div',
     activeFilters: {
       clearHref: '#',
-      clearText: 'Clear',
+      clearText: 'Clear All',
       filters: [
         {
           iconId: 'close',
@@ -27,156 +27,120 @@ module.exports = {
       ]
     },
     range: {
-      attributes: '',
-      class: '',
-      filter: {
-        wrapperClass: '',
-        input: {
-          upperClass: '',
-          lowerClass: '',
-          maxValue: '1000',
-          minValue: '0',
-          type: 'text'
+      default: false,
+      rangeEnabled: true,
+      id: 'range',
+      title: 'Range',
+      config: {
+        attributes: '',
+        class: '',
+        filter: {
+          wrapperClass: '',
+          input: {
+            upperClass: '',
+            lowerClass: '',
+            maxValue: '1000',
+            minValue: '0',
+            type: 'text'
+          },
+          suffix: {
+            lowerText: '$',
+            upperText: '$',
+            lowerClass: '',
+            upperClass: ''
+          }
         },
-        suffix: {
-          lowerText: '$',
-          upperText: '$',
-          lowerClass: '',
-          upperClass: ''
-        }
-      },
-      rangeInputLower: {
-        class: 'range-filter__input range-filter__input--lower',
-        label: {
-          class: 'range-filter__label',
-          text: 'Minimum value'
+        rangeInputLower: {
+          class: 'range-filter__input range-filter__input--lower',
+          label: {
+            class: 'range-filter__label',
+            text: 'Minimum value'
+          },
+          field: {
+            class: 'range-filter__field range-filter__field--lower',
+            id: 'range-filter-min',
+            name: 'range-filter-min',
+            placeholder: '',
+            value: '0'
+          }
         },
-        field: {
-          class: 'range-filter__field range-filter__field--lower',
-          id: 'range-filter-min',
-          name: 'range-filter-min',
-          placeholder: '',
-          value: '0'
-        }
-      },
-      rangeInputUpper: {
-        class: 'range-filter__input range-filter__input--upper',
-        label: {
-          class: 'range-filter__label',
-          text: 'Maximum value'
+        rangeInputUpper: {
+          class: 'range-filter__input range-filter__input--upper',
+          label: {
+            class: 'range-filter__label',
+            text: 'Maximum value'
+          },
+          field: {
+            class: 'range-filter__field range-filter__field--upper',
+            id: 'range-filter-max',
+            name: 'range-filter-max',
+            placeholder: '',
+            value: '1000'
+          }
         },
-        field: {
-          class: 'range-filter__field range-filter__field--upper',
-          id: 'range-filter-max',
-          name: 'range-filter-max',
-          placeholder: '',
-          value: '1000'
-        }
-      },
-      script: true,
-      showFloat: 0
-    },
-    checkboxes: [
-      {
-        id: 'checkbox',
-        name: 'checkbox-name',
-        label: {
-          text: 'Insulated',
-          class: ''
-        },
-        icon: {
-          class: ''
-        },
-        input: {
-          class: ''
-        }
-      },
-      {
-        id: 'checkbox1',
-        name: 'checkbox-name1',
-        label: {
-          text: 'Lightweight',
-          class: ''
-        },
-        icon: {
-          class: ''
-        },
-        input: {
-          class: ''
-        }
-      },
-      {
-        id: 'checkbox2',
-        name: 'checkbox-name2',
-        label: {
-          text: 'Value',
-          class: ''
-        },
-        icon: {
-          class: ''
-        },
-        input: {
-          class: ''
-        }
+        script: true,
+        showFloat: 0
       }
-    ],
+    },
     swatch: {
-      class: '',
-      attributes: '',
-      titleClass: '',
-      title: 'Choose your size',
-      options: [
-        {
-          class: '',
-          attributes: '',
-          text: 'XS'
-        },
-        {
-          class: 'swatch__option--active',
-          attributes: '',
-          text: 'S'
-        },
-        {
-          class: '',
-          attributes: '',
-          text: 'M'
-        },
-        {
-          class: '',
-          attributes: '',
-          text: 'L'
-        },
-        {
-          class: '',
-          attributes: '',
-          text: 'XL'
-        }
-      ]
+      default: true,
+      content: '[SWATCHES]',
+      id: 'default',
+      title: 'Swatch'
     },
     color: {
-      name: 'color',
-      class: '',
-      title: 'Choose color',
-      options: [
+      default: true,
+      content: '[COLOR]',
+      id: 'default',
+      title: 'Color'
+    },
+    checkbox: {
+      default: false,
+      checkboxEnabled: true,
+      id: 'checkbox',
+      title: 'Checkbox',
+      checkboxes: [
         {
-          class: 'swatch__option--color',
-          attributes: 'style="background: #000"',
-          text: ''
+          id: 'checkbox',
+          name: 'checkbox-name',
+          label: {
+            text: 'Insulated',
+            class: ''
+          },
+          icon: {
+            class: ''
+          },
+          input: {
+            class: ''
+          }
         },
         {
-          class: 'swatch__option--color',
-          attributes: 'style="background: #fff"',
-          text: ''
+          id: 'checkbox1',
+          name: 'checkbox-name1',
+          label: {
+            text: 'Lightweight',
+            class: ''
+          },
+          icon: {
+            class: ''
+          },
+          input: {
+            class: ''
+          }
         },
         {
-          class: 'swatch__option--color swatch__option--active',
-          attributes: 'style="background: #5cb85c"',
-          text: ''
-        },
-        {
-          class: 'swatch__option--color',
-          attributes: 'style="background: #0275d8"',
-          text: ''
+          id: 'checkbox2',
+          name: 'checkbox-name2',
+          label: {
+            text: 'Value',
+            class: ''
+          },
+          icon: {
+            class: ''
+          },
+          input: {
+            class: ''
+          }
         }
       ]
     }
