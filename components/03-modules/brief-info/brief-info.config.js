4857454530bd3422441d9439124691299eb4fe43
Janusz Janczy <janusz.janczy@snow.dog>
2017-11-20 16:37:47 +0100

    #36112: add brief-info module

---
+++ components/03-modules/brief-info/brief-info.config.js
@@ -0,0 +1,57 @@
+module.exports = {
+  context: {
+    items: [
+      {
+        icon: {
+          class: 'brief-info__icon brief-info__icon--delivery-truck',
+          iconId: 'shipping-time-truck',
+          svgClass: 'brief-info__svg brief-info__svg--delivery-truck'
+        },
+        link: {
+          class: '',
+          href: '#',
+          text: 'see details'
+        },
+        title: {
+          class: '',
+          tag: 'h3',
+          text: 'Shipping in 24h'
+        }
+      },
+      {
+        icon: {
+          class: 'brief-info__icon',
+          iconId: 'curved-arrows'
+        },
+        link: {
+          class: '',
+          href: '#',
+          text: 'see details'
+        },
+        title: {
+          class: '',
+          tag: 'h3',
+          text: 'Free returns'
+        }
+      },
+      {
+        icon: {
+          class: 'brief-info__icon',
+          iconId: 'shield'
+        },
+        link: {
+          class: '',
+          href: '#',
+          text: 'see details'
+        },
+        title: {
+          class: '',
+          tag: 'h3',
+          text: 'Secure payment'
+        },
+
+      }
+    ]
+
+  }
+};
