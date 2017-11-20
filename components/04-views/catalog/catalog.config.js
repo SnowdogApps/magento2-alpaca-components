70ccf89eddfe99fd94152b0d64c931b84dcc8fd5
Bartek Igielski <igloczek@gmail.com>
2017-11-20 23:32:43 +0100

    #35899 - Catalog grid view preview update

--- components/04-views/catalog/catalog.config.js
+++ components/04-views/catalog/catalog.config.js
@@ -1,14 +1,90 @@
 module.exports = {
+  title: 'Catalog',
   context: {
     product: {
       tag: 'li',
       class: 'products-grid__item',
-      name: 'Some product name',
-      price: '210$',
-      type: 'some attribute text',
+      badge: {
+        name: 'badge--new',
+        context: {
+          class: 'badge products-grid-item__badge',
+          text: 'NEW'
+        }
+      },
       image: {
-        src: '/images/product/product-img-230_180.png'
-      }
+        class: 'products-grid-item__image',
+        dataSrc: '/images/products-grid-item/product-1_320_312.jpg'
+      },
+      name: 'Some product name - very long name because that\'s important',
+      price: {
+        name: 'price',
+        context: {
+          regularPrice: '1 400',
+          specialPrice: '',
+          prefix: {
+            tag: '',
+            text: '$'
+          },
+          suffix: {
+            tag: '',
+            text: ''
+          }
+        }
+      },
+      primaryAction: {
+        tag: 'button',
+        text: 'Add to Cart',
+        class: 'button products-grid-item__primary-action'
+      },
+      swatches: [
+        {
+          name: 'swatch',
+          context: {
+            class: 'swatch--small swatch--image',
+            options: [
+              {
+                attributes: 'style="background-image: url(../../images/swatch/swatch-orange.jpg);"'
+              },
+              {
+                attributes: 'style="background-image: url(../../images/swatch/swatch-green.jpg);"'
+              },
+              {
+                class: 'swatch__option--active',
+                attributes: 'style="background-image: url(../../images/swatch/swatch-red.jpg);"'
+              },
+              {
+                attributes: 'style="background-image: url(../../images/swatch/swatch-white.jpg);"'
+              }
+            ]
+          }
+        },
+        {
+          name: 'swatch',
+          context: {
+            class: 'swatch--small',
+            attributes: '',
+            titleClass: '',
+            options: [
+              {
+                text: 'XS'
+              },
+              {
+                class: 'swatch__option--active',
+                text: 'S'
+              },
+              {
+                text: 'M'
+              },
+              {
+                text: 'L'
+              },
+              {
+                text: 'XL'
+              }
+            ]
+          }
+        }
+      ]
     }
   },
   variants: [
