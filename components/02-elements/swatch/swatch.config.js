93f561dbb999c93626dc1d13ed1e20be477eb9ca
Bartek Igielski <igloczek@gmail.com>
2017-11-20 23:28:13 +0100

    #35899 - Swatch component refactor

--- components/02-elements/swatch/swatch.config.js
+++ components/02-elements/swatch/swatch.config.js
@@ -1,92 +1,78 @@
 module.exports = {
-  collated: true,
   context: {
     class: '',
     attributes: '',
-    titleClass: '',
     heading: {
       tag: 'h5',
-      title: 'Size'
+      class: '',
+      text: 'Size'
     },
     options: [
       {
-        class: '',
-        attributes: '',
         text: 'XS'
       },
       {
         class: 'swatch__option--active',
-        attributes: '',
         text: 'S'
       },
       {
-        class: '',
-        attributes: '',
         text: 'M'
       },
       {
-        class: '',
-        attributes: '',
         text: 'L'
       },
       {
-        class: '',
-        attributes: '',
         text: 'XL'
       }
     ]
   },
   variants: [
     {
-      name: 'attribute',
+      name: 'small',
       context: {
-        heading: {
-          tag: 'h5',
-          title: 'Color'
-        },
+        class: 'swatch--small'
+      }
+    },
+    {
+      name: 'image',
+      context: {
+        class: 'swatch--image',
+        heading: false,
         options: [
           {
-            class: 'swatch__option--attributes',
-            attributes: 'style="background: url(../../images/swatch/swatch-orange.jpg); background-size: cover; background-position: center center;"'
+            attributes: 'style="background-image: url(../../images/swatch/swatch-orange.jpg);"'
           },
           {
-            class: 'swatch__option--attributes',
-            attributes: 'style="background: url(../../images/swatch/swatch-green.jpg); background-size: cover; background-position: center center;"'
+            attributes: 'style="background-image: url(../../images/swatch/swatch-green.jpg);"'
           },
           {
-            class: 'swatch__option--attributes swatch__option--active',
-            attributes: 'style="background: url(../../images/swatch/swatch-red.jpg); background-size: cover; background-position: center center;"'
+            class: 'swatch__option--active',
+            attributes: 'style="background-image: url(../../images/swatch/swatch-red.jpg);"'
           },
           {
-            class: 'swatch__option--attributes',
-            attributes: 'style="background: url(../../images/swatch/swatch-white.jpg); background-size: cover; background-position: center center;"'
+            attributes: 'style="background-image: url(../../images/swatch/swatch-white.jpg);"'
           }
         ]
       }
     },
     {
-      name: 'small-attribute',
+      name: 'small-image',
       context: {
-        class: 'swatch--small-attributes',
-        heading: {
-          title: ''
-        },
+        class: 'swatch--small swatch--image',
+        heading: false,
         options: [
           {
-            class: 'swatch__option--attributes',
-            attributes: 'style="background: url(../../images/swatch/swatch-orange.jpg); background-size: cover; background-position: center center;"'
+            attributes: 'style="background-image: url(../../images/swatch/swatch-orange.jpg);"'
           },
           {
-            class: 'swatch__option--attributes',
-            attributes: 'style="background: url(../../images/swatch/swatch-green.jpg); background-size: cover; background-position: center center;"'
+            attributes: 'style="background-image: url(../../images/swatch/swatch-green.jpg);"'
           },
           {
-            class: 'swatch__option--attributes swatch__option--active',
-            attributes: 'style="background: url(../../images/swatch/swatch-red.jpg); background-size: cover; background-position: center center;"'
+            class: 'swatch__option--active',
+            attributes: 'style="background-image: url(../../images/swatch/swatch-red.jpg);"'
           },
           {
-            class: 'swatch__option--attributes',
-            attributes: 'style="background: url(../../images/swatch/swatch-white.jpg); background-size: cover; background-position: center center;"'
+            attributes: 'style="background-image: url(../../images/swatch/swatch-white.jpg);"'
           }
         ]
       }
