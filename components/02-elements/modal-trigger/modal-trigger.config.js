c1cd45782b71ebdf6cd54da49b14069c2eaff2f1
Pawel Szafranski <pawel.szafranski@snow.dog>
2017-11-28 23:45:39 +0100

    #36545 - modal - added module with appropriate js script

---
+++ components/02-elements/modal-trigger/modal-trigger.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  context: {
+    buttonModalTrigger: {
+      tag: 'button',
+      class: 'modal-trigger',
+      text: 'Modal trigger button',
+      attributes: 'data-popuptrigger="modal-1" type="button"'
+    }
+  }
+};
---
+++ components/03-modules/modal/modal.js
@@ -0,0 +1,45 @@
+class Modal {
+  constructor() {
+    this.triggers = document.querySelectorAll('.modal-trigger');
+    this.init();
+  }
+
+  toggleModal(modalElement) {
+    modalElement.classList.toggle('modal--active')
+  }
+
+  setListeners() {
+    this.triggers.forEach(trigger => {
+      const triggerId           = trigger.dataset.popuptrigger,
+            modalElement        = document.querySelector(`.modal[data-modal=${triggerId}]`),
+            modalContentElement = modalElement.querySelector('.modal__content'),
+            closeButton         = modalElement.querySelector('.modal__close-button');
+
+      /// When the user clicks on trigger, open the modal
+      trigger.addEventListener('click',
+        () => this.toggleModal(modalElement)
+      );
+
+      // When the user clicks on button (x), close the modal
+      closeButton.addEventListener('click',
+        () => this.toggleModal(modalElement)
+      );
+
+      // When the user clicks anywhere outside of the modal, close the modal
+      window.addEventListener('click', (e) => {
+        if (e.target === modalElement
+          && modalElement.classList.contains('modal--active')
+          && !modalContentElement.contains(e.target)
+        ) {
+          this.toggleModal(modalElement)
+        }
+      });
+    })
+  }
+
+  init() {
+    this.setListeners();
+  }
+}
+
+new Modal();
