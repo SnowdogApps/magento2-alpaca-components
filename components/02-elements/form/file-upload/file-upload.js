9e180827956c28ef06f30a4e4fc310b78aeff5f9
Ania Karon <anna.karon@snow.dog>
2017-12-07 15:57:00 +0100

    #36913 update js script

--- components/02-elements/form/file-upload/file-upload.js
+++ components/02-elements/form/file-upload/file-upload.js
@@ -1,28 +1,30 @@
-'use strict'
+(function() { //eslint-disable-line func-names
+  'use strict'
 
-const components = [...document.querySelectorAll('.file-upload')];
+  const components = [...document.querySelectorAll('.file-upload')];
 
-components.forEach(component => {
-  const input  = component.querySelector('.file-upload__input'),
-        name   = component.querySelector('.file-upload__name'),
-        button = component.querySelector('span[role="button"]');
+  components.forEach(component => {
+    const input  = component.querySelector('.file-upload__input'),
+          name   = component.querySelector('.file-upload__name'),
+          button = component.querySelector('span[role="button"]');
 
-  input.addEventListener('change', file => {
-    const filePath = file.target.value,
-          fileName = filePath.split('\\').pop();
+    input.addEventListener('change', file => {
+      const filePath = file.target.value,
+            fileName = filePath.split('\\').pop();
 
-    if (fileName) {
-      name.innerHTML = fileName;
-    }
-    else {
-      name.innerHTML = name.dataset.defaultText;
-    }
-    name.focus();
+      if (fileName) {
+        name.innerHTML = fileName;
+      }
+      else {
+        name.innerHTML = name.dataset.defaultText;
+      }
+      name.focus();
+    });
+
+    button.addEventListener('keypress', event => {
+      if (event.which === 32 || event.which === 13) {
+        input.click();
+      }
+    });
   });
-
-  button.addEventListener('keypress', function(e){
-    if(e.which === 32 || e.which === 13){
-      input.click();
-    }
-  });
-})
+})();
