b0f13ad469240db4f60ef8a31de777997406f0ca
Ania Karon <anna.karon@snow.dog>
2017-12-07 15:42:23 +0100

    #36913 file upload accessibility features added

--- components/02-elements/form/file-upload/file-upload.js
+++ components/02-elements/form/file-upload/file-upload.js
@@ -3,8 +3,9 @@
 const components = [...document.querySelectorAll('.file-upload')];
 
 components.forEach(component => {
-  const input = component.querySelector('.file-upload__input'),
-        name  = component.querySelector('.file-upload__name');
+  const input  = component.querySelector('.file-upload__input'),
+        name   = component.querySelector('.file-upload__name'),
+        button = component.querySelector('span[role="button"]');
 
   input.addEventListener('change', file => {
     const filePath = file.target.value,
@@ -16,6 +17,12 @@ components.forEach(component => {
     else {
       name.innerHTML = name.dataset.defaultText;
     }
+    name.focus();
+  });
+
+  button.addEventListener('keypress', function(e){
+    if(e.which === 32 || e.which === 13){
+      input.click();
+    }
   });
 })
-
