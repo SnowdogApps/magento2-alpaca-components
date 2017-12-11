58cc0713317e94ccdbdc02661c938f67bef13feb
Pawel Szafranski <pawel.szafranski@snow.dog>
2017-12-12 00:14:52 +0100

    #36359 - catalog - removed useless code

--- components/03-modules/catalog-list-item/catalog-list-item.config.js
+++ components/03-modules/catalog-list-item/catalog-list-item.config.js
@@ -49,56 +49,7 @@ module.exports = {
       iconId: 'compare',
       iconClass: 'button__icon catalog-list-item__actions-secondary-button-icon',
       attributes: 'type="button" aria-label="button compare"'
-    },
-    swatches: [
-      {
-        name: 'swatch',
-        context: {
-          class: 'swatch--small swatch--image',
-          options: [
-            {
-              attributes: 'style="background-image: url(../../images/swatch/swatch-orange.jpg);"'
-            },
-            {
-              attributes: 'style="background-image: url(../../images/swatch/swatch-green.jpg);"'
-            },
-            {
-              class: 'swatch__option--active',
-              attributes: 'style="background-image: url(../../images/swatch/swatch-red.jpg);"'
-            },
-            {
-              attributes: 'style="background-image: url(../../images/swatch/swatch-white.jpg);"'
-            }
-          ]
-        }
-      },
-      {
-        name: 'swatch',
-        context: {
-          class: 'swatch--small',
-          attributes: '',
-          titleClass: '',
-          options: [
-            {
-              text: 'XS'
-            },
-            {
-              class: 'swatch__option--active',
-              text: 'S'
-            },
-            {
-              text: 'M'
-            },
-            {
-              text: 'L'
-            },
-            {
-              text: 'XL'
-            }
-          ]
-        }
-      }
-    ]
+    }
   },
   variants: [
     {
