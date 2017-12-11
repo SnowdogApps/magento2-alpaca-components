3ab12c87f8ad8d9ddcd02e94e88e7d768e3be936
Filip Kudła <kjugi@users.noreply.github.com>
2017-12-11 17:25:24 +0100

    Indentation fix

--- components/02-elements/rating/rating.config.js
+++ components/02-elements/rating/rating.config.js
@@ -1,17 +1,17 @@
 module.exports = {
   context: {
-      star: {
-          attributes: 'style="width: 72%"'
-      }
+    star: {
+      attributes: 'style="width: 72%"'
+    }
   },
   variants: [
     {
       name: 'rate',
       context: {
-         ratingScore: '71%',
-         star: {
-           attributes: ''
-         }
+        ratingScore: '71%',
+        star: {
+          attributes: ''
+        }
       }
     }
   ]
