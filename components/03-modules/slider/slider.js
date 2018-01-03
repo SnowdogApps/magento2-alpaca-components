cbcd00324fe73a00aaabef68be64e8001bbb9263
Filip Kudła <filip.kudla@snow.dog>
2018-01-03 16:07:06 +0100

    #37661 - Rewrite script for slider, avoid functions inside loop

--- components/03-modules/slider/slider.js
+++ components/03-modules/slider/slider.js
@@ -5,104 +5,125 @@
 
   const sliders = [...document.querySelectorAll('.slider')];
 
-  sliders.forEach(slider => {
-    const dataValues   = slider.dataset,
-          items        = slider.querySelectorAll('.slider__item').length,
-          dotContainer = slider.querySelector('.slider__dot-navigation');
+  class Slider {
+    constructor(slider, dataValues) {
+      this.active = 'slider__dot--active';
+      this.config = {
+        classNameFrame         : 'slider__handler',
+        classNameSlideContainer: 'slider__slides',
+        classNamePrevCtrl      : 'slider__prev',
+        classNameNextCtrl      : 'slider__next',
+        slidesToScroll         : 1
+      };
+      this.dataValues = dataValues;
+      this.slider = slider;
+      this.dotContainer = this.slider.querySelector('.slider__dot-navigation');
+      this.items = this.slider.querySelectorAll('.slider__item').length;
+      this.navigationContainer = this.slider.querySelector('.slider__navigation');
+      this.dotCount = 0;
 
-    let config = {
-      classNameFrame: 'slider__handler',
-      classNameSlideContainer: 'slider__slides',
-      classNamePrevCtrl: 'slider__prev',
-      classNameNextCtrl: 'slider__next',
-      slidesToScroll: 1
-    };
+      if (this.dotContainer) {
+        this.dots = this.dotContainer.childNodes;
 
-    Object.keys(dataValues).map((objectKey) => {
-      if (objectKey != 'slidesToScroll') {
-        if (!isNaN(parseInt(dataValues[objectKey]))) {
-          config[objectKey] = parseInt(dataValues[objectKey]);
-        }
-        else {
-          config[objectKey] = dataValues[objectKey];
-        }
+        this.slider.addEventListener('before.lory.init', () => {
+          this.cloneDots()
+          this.addDotListeners();
+        });
+
+        this.slider.addEventListener('after.lory.slide', (event) => {
+          this.dotClassToggle(event);
+        });
+
+        this.slider.addEventListener('on.lory.resize', () => {
+          this.lorySlider.slideTo(0);
+          this.dotContainer.innerHTML = '';
+
+          this.cloneDots();
+          this.addDotListeners();
+
+          this.dotContainer.childNodes[0].classList.add(this.dotActive);
+        });
       }
-    });
 
-    function cloneDots() {
-      const dots      = dotContainer.childNodes,
-            dotActive = 'slider__dot--active',
-            dotCount  = dotNumber(),
-            dotItem   = document.createElement('div');
+      Object.keys(this.dataValues).map((objectKey) => {
+        if (objectKey !== 'slidesToScroll') {
+          if (!isNaN(parseInt(this.dataValues[objectKey]))) {
+            this.config[objectKey] = parseInt(this.dataValues[objectKey]);
+          }
+          else {
+            this.config[objectKey] = this.dataValues[objectKey];
+          }
+        }
+      });
 
-      for (let i = 0; i < dotCount; i++) {
-        let clone = dotItem.cloneNode();
-        const nestedElement = document.createElement('span');
-
-        clone.classList.add('slider__dot');
-        clone.appendChild(nestedElement);
-        nestedElement.classList.add('slider__dot-inside');
-        dotContainer.appendChild(clone);
-      }
-
-      dots[0].classList.add(dotActive);
+      this.lorySlider = lory(this.slider, this.config);
     }
 
-    function addDotListeners() {
-      const dots = dotContainer.childNodes;
+    cloneDots() {
+      this.dotNumber();
+      const dotItem          = document.createElement('div'),
+            navigationHidden = 'slider__navigation--hidden';
+
+      if (this.dotCount > 1) {
+        this.navigationContainer.classList.remove(navigationHidden);
+
+        for (let i = 0; i < this.dotCount; i++) {
+          let clone = dotItem.cloneNode();
+          const nestedElement = document.createElement('span');
+
+          clone.classList.add('slider__dot');
+
+          clone.appendChild(nestedElement);
+          nestedElement.classList.add('slider__dot-inside');
+
+          this.dotContainer.appendChild(clone);
+        }
+
+        this.dotContainer.childNodes[0].classList.add(this.active);
+      }
+      else {
+        this.navigationContainer.classList.add(navigationHidden);
+      }
+    }
+
+    dotNumber() {
+      const sliderWidth     = this.slider.querySelector('.slider__slides').offsetWidth,
+            itemWidth       = this.slider.querySelector('.slider__item').offsetWidth,
+            visibleElements = Math.round(sliderWidth / itemWidth),
+            itemsLeft       = (this.items - visibleElements) + 1;
+
+      this.dotCount = (itemsLeft > 1) ? (this.items - visibleElements) + 1 : 1;
+
+      return this.dotCount;
+    }
+
+    addDotListeners() {
+      const dots = this.dotContainer.childNodes;
 
       dots.forEach(dot => {
         dot.addEventListener('click', (event) => {
           if (event.target.parentNode === dot) {
-            lorySlider.slideTo(Array.prototype.indexOf.call(dots, event.target.parentNode));
+            this.lorySlider.slideTo(Array.prototype.indexOf.call(dots, event.target.parentNode));
           }
           else {
-            lorySlider.slideTo(Array.prototype.indexOf.call(dots, event.target));
+            this.lorySlider.slideTo(Array.prototype.indexOf.call(dots, event.target));
           }
         });
       })
     }
 
-    function dotClassToggle(event) {
-      const dots      = dotContainer.childNodes,
-            dotActive = 'slider__dot--active';
-
-      dots.forEach(dot => {
-        dot.classList.remove(dotActive);
+    dotClassToggle(e) {
+      this.dotContainer.childNodes.forEach(dot => {
+        dot.classList.remove(this.active);
       });
 
-      dots[event.detail.currentSlide].classList.add(dotActive);
+      this.dotContainer.childNodes[e.detail.currentSlide].classList.add(this.active);
     }
+  }
 
-    function sliderResize() {
-      const dots      = dotContainer.childNodes,
-            dotActive = 'slider__dot--active';
+  sliders.forEach(slider => {
+    const dataValues = slider.dataset;
 
-      lorySlider.slideTo(0);
-      dotContainer.innerHTML = '';
-
-      cloneDots();
-      addDotListeners();
-
-      dots[0].classList.add(dotActive);
-    }
-
-    function dotNumber() {
-      const visibleElements = Math.round(slider.querySelector('.slider__slides').offsetWidth / slider.querySelector('.slider__item').offsetWidth);
-      let dotCount;
-
-      dotCount = ((items - visibleElements) + 1 > 1) ? (items - visibleElements) + 1 : 1;
-
-      return dotCount;
-    }
-
-    if (dotContainer) {
-      slider.addEventListener('before.lory.init', cloneDots);
-      slider.addEventListener('after.lory.init', addDotListeners);
-      slider.addEventListener('after.lory.slide', dotClassToggle);
-      slider.addEventListener('on.lory.resize', sliderResize);
-    }
-
-    const lorySlider = lory(slider, config); //eslint-disable-line one-var
+    new Slider(slider, dataValues);
   });
 }());
