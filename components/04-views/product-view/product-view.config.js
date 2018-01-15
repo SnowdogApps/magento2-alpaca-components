b6d14d0777177ca9026cbdc45be335f8b98b2e77
bartdominiak <smugers@gmail.com>
2018-01-15 13:36:17 +0100

    Tab bugs and clean-up (#306)
    * #37785 - added scope to take only first element of .tab tag
    
    * #37785 - added condition to icon and clean-up
    
    * #37785 - changed ":scope" to js condition
    
    * #37785 - deleted resize event
    
    * #37785 - empty line
    
    * #37785 - clean up (Cr fixes)
    
    * #37785 - Formatting changes

--- components/04-views/product-view/product-view.config.js
+++ components/04-views/product-view/product-view.config.js
@@ -209,6 +209,10 @@ module.exports = {
     tab: {
       script: true,
       class: 'product-view__tab',
+      icon: {
+        class: 'tab__icon',
+        iconId: 'angle-down'
+      },
       tabs: [
         {
           active: true,
@@ -217,10 +221,6 @@ module.exports = {
           content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pharetra ut magna ornare lacinia. Cras sodales elit ac pellentesque aliquet. Nulla nec viverra turpis. Mauris eget quam interdum, viverra enim eget, ultricies purus. Suspendisse eleifend, turpis id pretium consectetur, massa nunc suscipit elit, a porta neque neque sed ex. Curabitur nec ante non urna rhoncus semper. Maecenas id pulvinar erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque mollis sit amet quam id vestibulum. Nunc faucibus quam non venenatis laoreet. Pellentesque eu feugiat tellus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
           class: 'tab__title--active',
           contentClass: 'tab__content--active',
-          icon: {
-            class: 'tab__icon',
-            iconId: 'angle-down'
-          },
           table: {
             class: 'product-view__table',
             mainTags: [
@@ -287,20 +287,12 @@ module.exports = {
         {
           tabId: 'tab-2',
           title: 'More information',
-          content: 'Etiam orci ex, tristique at tempor quis, bibendum vel diam. Nullam vel mollis leo. Etiam facilisis mattis tortor, a ullamcorper nulla congue sed. Morbi non odio neque. In ut leo a elit consectetur fermentum aliquet non eros. In elementum vitae nulla sed semper. Nulla lacinia fermentum quam eu scelerisque. Sed et bibendum augue. Etiam sagittis enim eu ex aliquet ornare. Donec congue facilisis neque.',
-          icon: {
-            class: 'tab__icon',
-            iconId: 'angle-down'
-          }
+          content: 'Etiam orci ex, tristique at tempor quis, bibendum vel diam. Nullam vel mollis leo. Etiam facilisis mattis tortor, a ullamcorper nulla congue sed. Morbi non odio neque. In ut leo a elit consectetur fermentum aliquet non eros. In elementum vitae nulla sed semper. Nulla lacinia fermentum quam eu scelerisque. Sed et bibendum augue. Etiam sagittis enim eu ex aliquet ornare. Donec congue facilisis neque.'
         },
         {
           tabId: 'tab-3',
           title: 'Reviews (34)',
-          content: 'Pellentesque velit nisl, posuere ac erat nec, iaculis auctor magna. Donec ut elementum mauris. Sed in scelerisque lorem. Cras pretium laoreet vestibulum. In dui mauris, sagittis vitae rhoncus quis, ornare in enim. Vivamus pellentesque ligula sed quam sollicitudin lobortis. Etiam interdum euismod nisi a interdum. Nunc turpis urna, pharetra nec nunc sit amet, consequat porta orci. Suspendisse sit amet dignissim lacus. Quisque vel est pretium, aliquam lacus id, lobortis neque. Nullam non neque mi. Vivamus quis pulvinar mi, in elementum nulla. Etiam neque diam, suscipit vitae luctus et, sollicitudin in turpis. Proin vitae cursus augue, sit amet vestibulum mi. Mauris feugiat justo eget purus fringilla aliquam vitae a massa.',
-          icon: {
-            class: 'tab__icon',
-            iconId: 'angle-down'
-          }
+          content: 'Pellentesque velit nisl, posuere ac erat nec, iaculis auctor magna. Donec ut elementum mauris. Sed in scelerisque lorem. Cras pretium laoreet vestibulum. In dui mauris, sagittis vitae rhoncus quis, ornare in enim. Vivamus pellentesque ligula sed quam sollicitudin lobortis. Etiam interdum euismod nisi a interdum. Nunc turpis urna, pharetra nec nunc sit amet, consequat porta orci. Suspendisse sit amet dignissim lacus. Quisque vel est pretium, aliquam lacus id, lobortis neque. Nullam non neque mi. Vivamus quis pulvinar mi, in elementum nulla. Etiam neque diam, suscipit vitae luctus et, sollicitudin in turpis. Proin vitae cursus augue, sit amet vestibulum mi. Mauris feugiat justo eget purus fringilla aliquam vitae a massa.'
         }
       ]
     },
