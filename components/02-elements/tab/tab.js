c5023d512302af9406caa4636221c37c86af77a0
Bartek Igielski <igloczek@gmail.com>
2018-01-15 13:15:34 +0100

    #37785 - Formatting changes

--- components/02-elements/tab/tab.js
+++ components/02-elements/tab/tab.js
@@ -9,18 +9,22 @@
     const children = Array.from(element.children);
 
     children.forEach(item => {
-      let tabTitle = item.dataset.tab;
+      const tabTitle = item.dataset.tab;
 
       if (item.classList.contains('tab__title')) {
         item.addEventListener('click', () => {
           children.forEach((item) => {
             item.classList.remove(activeTitleClass);
             item.classList.remove(activeContentClass);
-            if (item.classList.contains('tab__content') &&
-               tabTitle === item.dataset.content) {
+            
+            if (
+              item.classList.contains('tab__content')
+              && tabTitle === item.dataset.content
+            ) {
               item.classList.add(activeContentClass);
             }
           });
+          
           item.classList.add(activeTitleClass);
         });
       }
