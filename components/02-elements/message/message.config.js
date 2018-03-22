6ede544becc2ebb00c5a3466d6157efe19d3c982
Filip Kudła <filip.kudla@snow.dog>
2018-03-22 12:59:29 +0100

    #39899 - Rebuild messsage element with using icon and change styles

--- components/02-elements/message/message.config.js
+++ components/02-elements/message/message.config.js
@@ -4,19 +4,22 @@ module.exports = {
     class: '',
     attributes: '',
     text: 'You added Jacket to your shopping cart.',
+    icon: 'warning'
   },
   variants: [
     {
       name: 'success',
       context: {
         class: 'message--success',
+        icon: 'check'
       }
     },
     {
       name: 'error',
       context: {
         class: 'message--error',
-        text: 'Please specify product\'s required options(s).'
+        text: 'Please specify product\'s required options(s).',
+        icon: 'error'
       }
     }
   ]
