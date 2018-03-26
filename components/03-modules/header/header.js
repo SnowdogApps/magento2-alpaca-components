1cfbd50df7c12bc42c3c452ab700713dd624488e
Pawel Szafranski <pawel.szafranski@snow.dog>
2018-03-26 17:34:39 +0200

    #40278 - side menu - fixed toggle js script and main styles

--- components/03-modules/header/header.js
+++ components/03-modules/header/header.js
@@ -4,21 +4,23 @@ class Header {
   constructor() {
     this.header = document.querySelector('.header')
     this.sideMenuContainer = this.header.querySelector('.side-menu');
-    this.menuTrigger = this.header.querySelectorAll('.header__menu-trigger');
+    this.sideMenuBg = this.sideMenuContainer.querySelector('.side-menu__bg');
+    this.menuTrigger = this.header.querySelector('.header__menu-trigger');
     this.init();
   }
 
   toggleSideMenu(el) {
-    el.currentTarget.classList.toggle('header__menu-trigger--active');
     this.header.classList.toggle('header--menu-open');
     this.sideMenuContainer.classList.toggle('side-menu--is-open');
   }
 
   setListeners() {
-    this.menuTrigger.forEach(el => {
-      el.addEventListener('click',
-        el => this.toggleSideMenu(el)
-      );
+    this.menuTrigger.addEventListener('click', () => {
+      this.toggleSideMenu();
+    });
+
+    this.sideMenuBg.addEventListener('click', () => {
+      this.toggleSideMenu();
     });
   }
 
