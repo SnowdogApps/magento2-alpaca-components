e306cdefda6e8c3fea70fbd727df9df864542292
Damian Fiałkiewicz <damian.fialkiewicz@snow.dog>
2018-03-27 15:05:05 +0200

    #40373 - Add quicksearch module with product list

---
+++ components/03-modules/quicksearch/quicksearch.config.js
@@ -0,0 +1,65 @@
+module.exports = {
+  context: {
+    results: {
+      text: 'Results for your request:',
+      typed: 'Ni'
+    },
+    close: {
+      tag: 'button',
+      class: 'button--icon quicksearch__close-button',
+      iconId: 'close',
+      iconClass: 'button__icon quicksearch__close-icon',
+      attributes: 'type="button" aria-label="Close quicksearch"',
+    },
+    product: [
+      {
+        image: {
+          class: 'quicksearch__product-image',
+          dataSrc: '/images/catalog-grid-item/product-1_320_312.jpg'
+        },
+        name: {
+          tag: 'h2',
+          class: '',
+          text: 'Lorem Ipsum is simply dummy...'
+        },
+        price: '$1684.00'
+      },
+      {
+        image: {
+          class: 'quicksearch__product-image',
+          dataSrc: '/images/catalog-grid-item/product-1_320_312.jpg'
+        },
+        name: {
+          tag: 'h2',
+          class: '',
+          text: 'Lorem Ipsum is simply dummy...'
+        },
+        price: '$1684.00'
+      },
+      {
+        image: {
+          class: 'quicksearch__product-image',
+          dataSrc: '/images/catalog-grid-item/product-1_320_312.jpg'
+        },
+        name: {
+          tag: 'h2',
+          class: '',
+          text: 'Lorem Ipsum is simply dummy...'
+        },
+        price: '$1684.00'
+      },
+      {
+        image: {
+          class: 'quicksearch__product-image',
+          dataSrc: '/images/catalog-grid-item/product-1_320_312.jpg'
+        },
+        name: {
+          tag: 'h2',
+          class: '',
+          text: 'Lorem Ipsum is simply dummy...'
+        },
+        price: '$1684.00'
+      }
+    ]
+  }
+}
