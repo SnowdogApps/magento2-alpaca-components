43ddeca6ef432407159d17e0434179b442bd9b03
Damian Fiałkiewicz <damian.fialkiewicz@snow.dog>
2018-08-22 08:44:34 +0200

    #44927 - Fix rating stars size

--- components/03-modules/review/review.config.js
+++ components/03-modules/review/review.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   context: {
     rating: {
-      percentFilled: 'style="width: 80%;"',
-      percent: '<span>80%</span>',
+      ariaLabel: 'Average rating 60%',
+      star: {
+        attributes: 'style="width: 60%"'
+      }
     },
     title: 'Lorem Ipsum',
     content: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lacus odio, rutrum a augue nec, varius vehicula odio. Praesent at nibh vehicula libero pellentesque vulputate. Ut malesuada dolor eget purus condimentum, eget porttitor est luctus. Donec tristique, tortor a iaculis mollis, ex erat gravida nunc, quis viverra tortor neque quis eros. Pellentesque lorem sapien, consequat non vestibulum ac, pellentesque vitae risus. Nunc eleifend magna id fringilla semper. </p>',
