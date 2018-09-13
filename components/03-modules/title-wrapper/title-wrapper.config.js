4edaf9bd13a0f2d88610e52bf2c789d539053437
Pawel Szafranski <pawel.szafranski@snow.dog>
2018-09-13 17:19:55 +0200

    #45316 - updated title wrapper module

--- components/03-modules/title-wrapper/title-wrapper.config.js
+++ components/03-modules/title-wrapper/title-wrapper.config.js
@@ -4,6 +4,12 @@ module.exports = {
       text: 'main title',
       tag: 'h1',
       class: 'heading heading--page'
+    },
+    button: {
+      tag: 'button',
+      class: 'button button--secondary',
+      attributes: 'type="button"',
+      text: 'I am a button'
     }
   }
 };
