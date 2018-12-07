05f00f8d0117b8b53b1e7f5fff37ef33d858a938
Robert Wozniak <robert.wozniak@snow.dog>
2018-12-07 10:52:39 +0100

    #47975 - Remove unused offset from grid configuration

--- components/01-globals/grid/grid.config.js
+++ components/01-globals/grid/grid.config.js
@@ -55,25 +55,6 @@ module.exports = {
       }
     },
     {
-      name: 'offsets',
-      context: {
-        title: 'Offsets',
-        grids: [
-          ['col-xs-offset-11 col-xs-1'],
-          ['col-xs-offset-10 col-xs-2'],
-          ['col-xs-offset-9 col-xs-3'],
-          ['col-xs-offset-8 col-xs-4'],
-          ['col-xs-offset-7 col-xs-5'],
-          ['col-xs-offset-6 col-xs-6'],
-          ['col-xs-offset-5 col-xs-7'],
-          ['col-xs-offset-4 col-xs-8'],
-          ['col-xs-offset-3 col-xs-9'],
-          ['col-xs-offset-2 col-xs-10'],
-          ['col-xs-offset-1 col-xs-11']
-        ]
-      }
-    },
-    {
       name: 'auto Width',
       context: {
         title: 'Auto Width',
