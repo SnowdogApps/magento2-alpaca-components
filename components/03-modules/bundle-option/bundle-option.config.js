cd9d13c524bb712a75781fee28ab75af1015fec1
Damian Fiałkiewicz <damian.fialkiewicz@snow.dog>
2018-12-28 14:02:15 +0100

    #40516 - Add bundle-option variants, update styles

--- components/03-modules/bundle-option/bundle-option.config.js
+++ components/03-modules/bundle-option/bundle-option.config.js
@@ -1,54 +1,100 @@
 module.exports = {
+  default: 'radio',
   context: {
     title: {
       tag: 'h4',
       message: 'Sprite Stasis Ball'
     },
-    checkboxes: [
-      {
-        content: 'radio',
-        config: {
-          name: 'bundleOption1'
-        }
-      }
-    ],
     qty: {
-      class: 'bundle-option__input',
+      class: 'bundle-option__qty',
       label: {
         class: 'bundle-option__label',
         text: 'Qty:'
       },
       field: {
         type: 'number',
-        class: 'bundle-option__input-field',
+        class: 'bundle-option__qty-input',
         id: 'qty1',
         name: 'qty1',
         placeholder: '',
         attributes: 'min="0"'
       }
     },
-    fields: [
-      {
-        content: 'radio',
-        config: {
-          name: 'bundleOption1',
-          legend: false,
+    radio: {
+      legend: false,
+      class: 'bundle-option__radio',
+      options: [
+        {
+          id: 'id1',
+          label: 'Sprite Stasis Ball 55 cm'
+        },
+        {
+          id: 'id2',
+          label: 'Sprite Stasis Ball 65 cm'
+        },
+        {
+          id: 'id3',
+          label: 'Sprite Stasis Ball 75 cm'
+        }
+      ]
+    }
+  },
+  variants: [
+    {
+      name: 'checkbox',
+      context: {
+        title: {
+          message: 'Sprite Yoga Strap',
+          tag: 'h4'
+        },
+        checkboxes: [
+          {
+            id: 'checkbox-1',
+            name: 'checkbox-name-1',
+            label: {
+              text: '1 x Sprite Yoga Strap 6 foot',
+            }
+          },
+          {
+            id: 'checkbox-2',
+            name: 'checkbox-name-2',
+            label: {
+              text: '1 x Sprite Yoga Strap 8 foot',
+            }
+          }
+        ],
+        qty: false
+      }
+    },
+    {
+      name: 'select',
+      context: {
+        title: {
+          message: 'Sprite Foam Roller',
+          tag: 'h4'
+        },
+        select: {
+          class: 'bundle-option__select',
+          label: {
+            class: 'label--hidden'
+          },
+          field: {
+            class: 'bundle-option__select'
+          },
           options: [
             {
-              id: 'id1',
-              label: 'Sprite Stasis Ball 55 cm',
+              text: 'Sprite Foam Roller'
             },
             {
-              id: 'id2',
-              label: 'Sprite Stasis Ball 65 cm',
+              text: 'Sprite Foam Roller S'
             },
             {
-              id: 'id3',
-              label: 'Sprite Stasis Ball 75 cm',
+              text: 'Sprite Foam Roller XL',
+              attributes: false
             }
           ]
         }
       }
-    ]
-  }
+    }
+  ]
 }
