aea4017d784f161709c0c4b17cae4e48a9d2a4f4
Jarek Krupiński <jarek.krupinski@snow.dog>
2019-07-29 15:03:38 +0200

    #51564 - Add list variant

--- components/02-elements/skip-nav/skip-nav.config.js
+++ components/02-elements/skip-nav/skip-nav.config.js
@@ -3,5 +3,22 @@ module.exports = {
     text: 'Skip to content',
     anchor: '#maincontent',
     script: true
-  }
+  },
+  variants: [
+    {
+      name: 'list',
+      context: {
+        anchor: [
+          {
+            value: '#header',
+            text: 'Go to top'
+          },
+          {
+            value: '#maincontent',
+            text: 'Go to main content'
+          }
+        ]
+      }
+    }
+  ]
 }
